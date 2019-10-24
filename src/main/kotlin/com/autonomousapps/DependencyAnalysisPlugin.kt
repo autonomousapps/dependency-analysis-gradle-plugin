f16cfa6c6aad8c523a783602dbcc430ec1478ef2
Tony Robalik <tony.robalik@gmail.com>
2019-10-24 01:02:34 -0700

    Major rewrite underway

--- src/main/kotlin/com/autonomousapps/DependencyAnalysisPlugin.kt
+++ src/main/kotlin/com/autonomousapps/DependencyAnalysisPlugin.kt
@@ -12,20 +12,26 @@ import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.component.ComponentIdentifier
-import org.gradle.api.artifacts.component.ComponentSelector
-import org.gradle.api.artifacts.component.LibraryBinaryIdentifier
 import org.gradle.api.artifacts.component.ModuleComponentIdentifier
 import org.gradle.api.artifacts.component.ProjectComponentIdentifier
 import org.gradle.api.artifacts.result.DependencyResult
 import org.gradle.api.artifacts.result.ResolutionResult
 import org.gradle.api.artifacts.result.ResolvedComponentResult
 import org.gradle.api.artifacts.result.ResolvedDependencyResult
-import org.gradle.api.artifacts.result.UnresolvedDependencyResult
 import org.gradle.api.attributes.Attribute
 import org.objectweb.asm.ClassReader
 import java.io.File
 import java.util.Locale
 import java.util.zip.ZipFile
+
+private const val PATH_ROOT = "class-analysis"
+private const val PATH_ALL_USED_CLASSES = "$PATH_ROOT/all-used-classes.txt"
+private const val FILE_ALL_ARTIFACTS = "all-artifacts.txt"
+private const val FILE_ALL_ARTIFACTS_PRETTY = "all-artifacts-pretty.txt"
+private const val PATH_ALL_ARTIFACTS = "$PATH_ROOT/$FILE_ALL_ARTIFACTS"
+private const val PATH_ALL_DECLARED_DEPS = "$PATH_ROOT/all-declared-dependencies.txt"
+private const val PATH_ALL_DECLARED_DEPS_PRETTY = "$PATH_ROOT/all-declared-dependencies-pretty.txt"
+private const val PATH_USED_TRANSITIVE_DEPS = "$PATH_ROOT/used-transitive-dependencies.txt"
 
 @Suppress("unused")
 class DependencyAnalysisPlugin : Plugin<Project> {
@@ -45,9 +51,9 @@ class DependencyAnalysisPlugin : Plugin<Project> {
         }
     }
 
-    private fun Project.analyzeAndroidLibraryDependencies() {
-        // 1.
-        // This produces a report that lists all of the used classes (FQCN) in the project
+    // 1.
+    // This produces a report that lists all of the used classes (FQCN) in the project
+    private fun Project.registerClassAnalysisTasks() {
         convention.findByType(LibraryExtension::class.java)!!.libraryVariants.all {
             logger.quiet("lib variant: $name")
 
@@ -57,61 +63,68 @@ class DependencyAnalysisPlugin : Plugin<Project> {
             val bundleTask = tasks.named("bundleLibCompile$name", BundleLibraryClasses::class.java)
             tasks.register("listClassesFor$name", ClassAnalysisTask::class.java) {
                 jar.set(bundleTask.flatMap { it.output })
-                output.set(layout.buildDirectory.file("cloc/class-analysis/all-used-classes.txt"))
+                output.set(layout.buildDirectory.file(PATH_ALL_USED_CLASSES))
             }
         }
+    }
 
-        // 2.
-        // This is awful spike code that is intended to produce a report of all the declared classes (FQCN) depended-on by the project
-        // These are obviously not necessarily actually used.
-        var dependencyArtifacts: Set<Dep>? = null
+    // 2.
+    // Produces a report that lists all direct and transitive dependencies, their artifacts, and component type (library
+    // vs project)
+    // TODO currently sucks because:
+    // 1. Will run every time assembleDebug runs
+    // 2. Does IO during configuration
+    private fun Project.resolveCompileClasspathArtifacts() {
         configurations.all {
+            // TODO need to reconsider how to get the correct configuration/classpath name
+            // compileClasspath has the correct artifacts
             if (name == "debugCompileClasspath") {
                 // This will gather all the resolved artifacts attached to the debugRuntimeClasspath INCLUDING transitives
                 incoming.afterResolve {
-                    //val a = deps.artifactView {
-//                        it.attributes.attribute(Attribute.of("artifactType", String::class.java), "jar") // TODO this is producing paths to files which do not exist?
-//                    }.artifacts.artifacts
-
                     val artifacts = artifactView {
                         attributes.attribute(Attribute.of("artifactType", String::class.java), "android-classes")
                     }.artifacts.artifacts
+                        .map { Artifact(componentIdentifier = it.id.componentIdentifier, file = it.file) }
+                        .toSet()
 
-//                    logger.debug(("debugRuntimeClasspath artifacts\n${artifacts.joinToString(separator = "\n")}")
-
-                    fun ComponentIdentifier.asString(): String {
-                        return when (this) {
-                            is ProjectComponentIdentifier -> projectPath
-                            is ModuleComponentIdentifier -> moduleIdentifier.toString()
-                            else -> throw GradleException("'This shouldn't happen'")
+                    val artifactsFileRoot = File(project.file(buildDir), PATH_ROOT).apply { mkdirs() }
+                    val artifactsFile = File(artifactsFileRoot, FILE_ALL_ARTIFACTS).also {
+                        if (it.exists()) {
+                            it.delete()
+                        }
+                    }
+                    val artifactsFilePretty = File(artifactsFileRoot, FILE_ALL_ARTIFACTS_PRETTY).also {
+                        if (it.exists()) {
+                            it.delete()
                         }
                     }
 
-                    dependencyArtifacts = artifacts.map {
-                        Dep(
-                            // TODO just set the componentIdentifier and let the class work out the identifier and componentType
-                            identifier = it.id.componentIdentifier.asString(),
-                            componentType = ComponentType.of(it.id.componentIdentifier),
-                            file = it.file
-                        )
-                    }.toSet()
-//                    logger.debug(("DependencyWithFiles:\n${d.joinToString("\n")}")
+                    artifactsFile.writeText(artifacts.toJson())
+                    artifactsFilePretty.writeText(artifacts.toPrettyString())
                 }
             }
         }
+    }
+
+    private fun Project.analyzeAndroidLibraryDependencies() {
+        registerClassAnalysisTasks()
+        resolveCompileClasspathArtifacts()
+        
 
         // Get top-level dependencies. Necessary (?) to determine which deps are transitive.
         val confs = tasks.register("confs")
-        val confs2 = tasks.register("confs2")
         val unused = tasks.register("unused")
         afterEvaluate {
-            // Update dependencyArtifacts with `isTransitive` value
+            // 3. Update dependencyArtifacts with `isTransitive` value
             confs.configure {
                 dependsOn(tasks.named("assembleDebug"))
 
                 doLast {
-                    logger.debug("===Traversing dependencies===")
+                    // Step 1
+                    val artifactsFile = layout.buildDirectory.file(PATH_ALL_ARTIFACTS).get().asFile
+                    val artifacts = artifactsFile.readText().fromJsonList<Artifact>()
 
+                    // runtime classpath will give me only the direct dependencies
                     val conf = configurations.getByName("debugRuntimeClasspath")
                     val result: ResolutionResult = conf.incoming.resolutionResult
                     val root: ResolvedComponentResult = result.root
@@ -119,83 +132,70 @@ class DependencyAnalysisPlugin : Plugin<Project> {
                     val dependencies: Set<DependencyResult> = root.dependencies
                     val allDependencies: Set<DependencyResult> = result.allDependencies
 
-                    val deps = traverseDependencies(0, dependencies)
+                    val deps = traverseDependencies(dependencies)
 
-                    dependencyArtifacts!!.forEach { dep ->
+                    artifacts.forEach { dep ->
                         dep.apply {
                             isTransitive = !deps.any { it.identifier == dep.identifier }
-                            file = dep.file
                         }
                     }
-//                  logger.debug(("DepWithFile2\n${d!!.joinToString("\n")}")
-                }
-            }
-            // end confs.configure {}
 
-            // Generate list of all classes declared by all dependencyArtifacts
-            confs2.configure {
-                    dependsOn(confs)
+                    // Step 2
+                    // Generate list of all classes declared by all dependencyArtifacts
+                    val output = layout.buildDirectory.file(PATH_ALL_DECLARED_DEPS).get().asFile
+                    output.delete()
 
-                    doLast {
-                        val output =
-                            layout.buildDirectory.file("cloc/class-analysis/all-declared-dependencies.txt").get().asFile
-                        output.delete()
+                    // TODO remove
+                    val outputPretty = layout.buildDirectory.file(PATH_ALL_DECLARED_DEPS_PRETTY).get().asFile
+                    outputPretty.delete()
 
-                        // TODO remove
-                        val outputPretty =
-                            layout.buildDirectory.file("cloc/class-analysis/all-declared-dependencies-pretty.txt").get()
-                                .asFile
-                        outputPretty.delete()
+                    val libraries = artifacts.filter {
+                        if (!it.file!!.exists()) {
+                            logger.error("File doesn't exist for dep $it")
+                        }
+                        it.file!!.exists()
+                    }.map { dep ->
+                        val z = ZipFile(dep.file)
 
-                        val libraries = dependencyArtifacts!!.filter {
-                            if (!it.file!!.exists()) {
-                                logger.error("File doesn't exist for dep $it")
+                        val classes = z.entries().toList()
+                            .filterNot { it.isDirectory }
+                            .filter { it.name.endsWith(".class") }
+                            .map { classEntry ->
+                                val classNameCollector = ClassNameCollector(logger) // was ClassPrinter
+                                val reader = ClassReader(z.getInputStream(classEntry).readBytes())
+                                reader.accept(classNameCollector, 0)
+                                classNameCollector
                             }
-                            it.file!!.exists()
-                        }.map { dep ->
-                            val z = ZipFile(dep.file)
+                            .mapNotNull { it.className }
+                            .filterNot {
+                                // Filter out `java` packages, but not `javax`
+                                it.startsWith("java/")
+                            }
+                            .toSet()
+                            .map { it.replace("/", ".") }
+                            .sorted()
 
-                            val classes = z.entries().toList()
-                                .filterNot { it.isDirectory }
-                                .filter { it.name.endsWith(".class") }
-                                .map { classEntry ->
-                                    val classNameCollector = ClassNameCollector(logger) // was ClassPrinter
-                                    val reader = ClassReader(z.getInputStream(classEntry).readBytes())
-                                    reader.accept(classNameCollector, 0)
-                                    classNameCollector
-                                }
-                                .mapNotNull { it.className }
-                                .filterNot {
-                                    // Filter out `java` packages, but not `javax`
-                                    it.startsWith("java/")
-                                }
-                                .toSet()
-                                .map { it.replace("/", ".") }
-                                .sorted()
+                        Library(dep.identifier, dep.isTransitive!!, classes)
+                    }.sorted()
 
-                            Library(dep.identifier, dep.isTransitive!!, classes)
-                        }.sorted()
-
-                        output.writeText(libraries.toJson())
-                        outputPretty.writeText(libraries.toPrettyString())
-                    }
+                    output.writeText(libraries.toJson())
+                    outputPretty.writeText(libraries.toPrettyString())
+                }
             }
 
             unused.configure {
-                dependsOn(confs2, tasks.named("listClassesForDebug"))
+                dependsOn(confs, tasks.named("listClassesForDebug"))
 
                 doLast {
                     // Inputs
                     val decl =
-                        layout.buildDirectory.file("cloc/class-analysis/all-declared-dependencies.txt").get().asFile
-                    val used = layout.buildDirectory.file("cloc/class-analysis/all-used-classes.txt").get().asFile
+                        layout.buildDirectory.file(PATH_ALL_DECLARED_DEPS).get().asFile
+                    val used = layout.buildDirectory.file(PATH_ALL_USED_CLASSES).get().asFile
 
-                    val outputUnused =
-                        layout.buildDirectory.file("cloc/class-analysis/used-transitive-dependencies.txt").get().asFile
+                    val outputUnused = layout.buildDirectory.file(PATH_USED_TRANSITIVE_DEPS).get().asFile
                     outputUnused.delete()
 
-                    val outputUsedTransitives =
-                        layout.buildDirectory.file("cloc/class-analysis/used-transitive-dependencies.txt").get().asFile
+                    val outputUsedTransitives = layout.buildDirectory.file(PATH_USED_TRANSITIVE_DEPS).get().asFile
                     outputUsedTransitives.delete()
 
                     val declaredLibs = decl.readText().fromJsonList<Library>()
@@ -261,66 +261,37 @@ class DependencyAnalysisPlugin : Plugin<Project> {
     }
 }
 
-private fun traverseDependencies(level: Int, results: Set<DependencyResult>): Set<Dep> {
+private fun traverseDependencies(results: Set<DependencyResult>): Set<Artifact> = results
+    .filterIsInstance<ResolvedDependencyResult>()
+    .map { result ->
+        val componentResult = result.selected
 
-    fun calculateIndentation(level: Int) = "     ".repeat(level)
-
-    val deps = mutableSetOf<Dep>()
-
-    results.forEach { result ->
-        if (result is ResolvedDependencyResult) {
-            val componentResult: ResolvedComponentResult = result.selected
-            // LibraryBinaryIdentifier, ModuleComponentIdentifier, ProjectComponentIdentifier
-            val componentIdentifier: ComponentIdentifier = componentResult.id
-            val compType = when (componentIdentifier) {
-                is LibraryBinaryIdentifier -> "lib"
-                is ModuleComponentIdentifier -> "mod"
-                is ProjectComponentIdentifier -> "pro"
-                else -> throw GradleException("'This should never happen'")
-            }
-
-            val node =
-                "${calculateIndentation(level)}- ($compType) ${componentIdentifier.displayName} (${componentResult.selectionReason})"
-//            println(node)
-//            traverseDependencies(level + 1, componentResult.dependencies)
-
-            if (componentIdentifier is ProjectComponentIdentifier) {
-                deps.add(Dep(identifier = componentIdentifier.projectPath, componentType = ComponentType.PROJECT))
-            } else if (componentIdentifier is ModuleComponentIdentifier) {
-                deps.add(
-                    Dep(
-                        identifier = componentIdentifier.moduleIdentifier.toString(),
-                        componentType = ComponentType.LIBRARY
-                    )
-                )
-            }
-        } else if (result is UnresolvedDependencyResult) {
-            val componentSelector: ComponentSelector = result.attempted
-            val node = "${calculateIndentation(level)}- ${componentSelector.displayName} (failed)"
-            println(node)
+        when (val componentIdentifier = componentResult.id) {
+            is ProjectComponentIdentifier -> Artifact(componentIdentifier)
+            is ModuleComponentIdentifier -> Artifact(componentIdentifier)
+            else -> throw GradleException("Unexpected ComponentIdentifier type: ${componentIdentifier.javaClass.simpleName}")
         }
-    }
+    }.toSet()
 
-    return deps
-}
-
-data class Dep(
+data class Artifact(
     val identifier: String,
     val componentType: ComponentType,
     var isTransitive: Boolean? = null,
     var file: File? = null
 ) {
-    data class Builder(
-        private var identifier: String? = null,
-        private var componentType: ComponentType? = null,
-        private var isTransitive: Boolean? = null,
-        private var file: File? = null
-    ) {
-        fun identifier(identifier: String) = apply { this.identifier = identifier }
-        fun componentType(componentType: ComponentType) = apply { this.componentType = componentType }
-        fun isTransitive(isTransitive: Boolean) = apply { this.isTransitive = isTransitive }
-        fun file(file: File) = apply { this.file = file }
-        fun build() = Dep(identifier!!, componentType!!, isTransitive!!, file!!)
+
+    constructor(componentIdentifier: ComponentIdentifier, file: File? = null) : this(
+        identifier = componentIdentifier.asString(),
+        componentType = ComponentType.of(componentIdentifier),
+        file = file
+    )
+}
+
+private fun ComponentIdentifier.asString(): String {
+    return when (this) {
+        is ProjectComponentIdentifier -> projectPath
+        is ModuleComponentIdentifier -> moduleIdentifier.toString()
+        else -> throw GradleException("Cannot identify ComponentIdentifier subtype. Was ${javaClass.simpleName}")
     }
 }
 
--- src/main/kotlin/com/autonomousapps/internal/MoshiAdapterFactory.kt
+++ src/main/kotlin/com/autonomousapps/internal/MoshiAdapterFactory.kt
@@ -1,13 +1,17 @@
 package com.autonomousapps.internal
 
+import com.squareup.moshi.FromJson
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.ToJson
 import com.squareup.moshi.Types.newParameterizedType
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import java.io.File
 
 internal val MOSHI: Moshi by lazy {
     Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
+        .add(TypeAdapters())
         .build()
 }
 
@@ -17,6 +21,11 @@ internal inline fun <reified T> getJsonAdapter(): JsonAdapter<T> {
 
 internal inline fun <reified T> getJsonListAdapter(): JsonAdapter<List<T>> {
     val type = newParameterizedType(List::class.java, T::class.java)
+    return MOSHI.adapter(type)
+}
+
+internal inline fun <reified T> getJsonSetAdapter(): JsonAdapter<Set<T>> {
+    val type = newParameterizedType(Set::class.java, T::class.java)
     return MOSHI.adapter(type)
 }
 
@@ -34,4 +43,15 @@ internal inline fun <reified T> String.fromJsonList(): List<T> {
 
 internal inline fun <reified T> List<T>.toPrettyString(): String {
     return getJsonListAdapter<T>().indent("  ").toJson(this)
+}
+
+internal inline fun <reified T> Set<T>.toPrettyString(): String {
+    return getJsonSetAdapter<T>().indent("  ").toJson(this)
+}
+
+internal class TypeAdapters {
+
+    @ToJson fun fileToJson(file: File) = file.absolutePath
+    @FromJson fun fileFromJson(absolutePath: String) = File(absolutePath)
+
 }
