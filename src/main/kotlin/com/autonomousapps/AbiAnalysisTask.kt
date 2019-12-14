da5fa6fc16cdd26e5efd649c8b62f4c92c7a1df8
Tony Robalik <tony@gradle.com>
2019-12-14 11:47:30 -0800

    Working code.

--- src/main/kotlin/com/autonomousapps/AbiAnalysisTask.kt
+++ src/main/kotlin/com/autonomousapps/AbiAnalysisTask.kt
@@ -10,6 +10,7 @@ import com.autonomousapps.internal.kotlin.getBinaryAPI
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.model.ObjectFactory
+import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Classpath
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.OutputFile
@@ -21,6 +22,7 @@ import org.slf4j.LoggerFactory
 import java.util.jar.JarFile
 import javax.inject.Inject
 
+@CacheableTask
 open class AbiAnalysisTask @Inject constructor(
     objects: ObjectFactory,
     private val workerExecutor: WorkerExecutor
@@ -42,21 +44,14 @@ open class AbiAnalysisTask @Inject constructor(
 
     @TaskAction
     fun action() {
-//        // Inputs
-//        val jarFile = jar.get().asFile
-//        val components = dependencies.get().asFile.readText().fromJsonList<Component>()
-//
-//        // Outputs
-//        val reportFile = output.get().asFile
-//
-//        // Cleanup prior execution
-//        reportFile.delete()
-
         workerExecutor.noIsolation().submit(AbiAnalysisWorkAction::class.java) {
             jar.set(this@AbiAnalysisTask.jar)
             dependencies.set(this@AbiAnalysisTask.dependencies)
             output.set(this@AbiAnalysisTask.output)
         }
+        workerExecutor.await()
+
+        logger.quiet("These are your API dependencies:\n${output.get().asFile.readLines().joinToString(prefix = "- ", separator = "\n- ")}")
     }
 }
 
@@ -81,11 +76,40 @@ abstract class AbiAnalysisWorkAction : WorkAction<AbiAnalysisParameters> {
         // Cleanup prior execution
         reportFile.delete()
 
-        reportFile.bufferedWriter().use {
-            getBinaryAPI(JarFile(jarFile)).filterOutNonPublic().dump(it)
-        }
-//            .map { classSignature ->
-//                classSignature
-//            }
+//        reportFile.bufferedWriter().use {
+//            getBinaryAPI(JarFile(jarFile)).filterOutNonPublic().dump(it)
+//        }
+        val apiDependencies = getBinaryAPI(JarFile(jarFile))
+            .filterOutNonPublic()
+            .flatMap { classSignature ->
+                val superTypes = classSignature.supertypes
+                val memberTypes = classSignature.memberSignatures.map {
+                    // descriptor, e.g. `(JLjava/lang/String;JI)Lio/reactivex/Single;`
+                    // This one takes a long, a String, a long, and an int, and returns a Single
+                    it.desc
+                }.flatMap {
+                    DESC_REGEX.findAll(it).allItems()
+                }
+                superTypes + memberTypes
+            }.map {
+                it.replace("/", ".")
+            }.mapNotNull { fqcn ->
+                components.find { component ->
+                    component.classes.contains(fqcn)
+                }?.identifier
+            }.toSortedSet()
+
+        reportFile.writeText(apiDependencies.joinToString("\n"))
     }
 }
+
+// TODO this is repeated in asm.kt. Should re-use.
+private val DESC_REGEX = """L(\w[\w/$]+);""".toRegex()
+
+private fun Sequence<MatchResult>.allItems(): List<String> =
+    flatMap { matchResult ->
+        val groupValues = matchResult.groupValues
+        // Ignore the 0th element, as it is the entire match
+        if (groupValues.isNotEmpty()) groupValues.subList(1, groupValues.size).asSequence()
+        else emptySequence()
+    }.toList()
