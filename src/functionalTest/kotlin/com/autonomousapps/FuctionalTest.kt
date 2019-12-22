b119bfe712464396ed94a6a349b6f7417c976574
Tony Robalik <trobalik@hiya.com>
2019-12-21 17:55:28 -0800

    Functional tests are now working (although maybe fragile).

--- src/functionalTest/kotlin/com/autonomousapps/FuctionalTest.kt
+++ src/functionalTest/kotlin/com/autonomousapps/FuctionalTest.kt
@@ -1,6 +1,7 @@
 package com.autonomousapps
 
-import java.io.File
+import com.autonomousapps.utils.AndroidProject
+import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
 import kotlin.test.Test
 import kotlin.test.assertTrue
@@ -10,151 +11,15 @@ class FunctionalTest {
 
     @Test fun `can assemble app`() {
         // Setup the test build
-        val projectDir = File("build/functionalTest")
-        projectDir.mkdirs()
-        projectDir.resolve("settings.gradle").writeText("""
-            rootProject.name = 'real-app'
-            include(':app')
-        """.trimIndent()
-        )
-        projectDir.resolve("build.gradle").writeText("""
-            buildscript {
-                repositories {
-                    google()
-                    jcenter()
-                }
-                dependencies {
-                    classpath 'com.android.tools.build:gradle:3.5.3'
-                    classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61"
-                }
-            }
-            subprojects {
-                repositories {
-                    google()
-                    jcenter()
-                }
-            }
-        """.trimIndent()
-        )
-        val appDir = projectDir.resolve("app")
-        appDir.mkdirs()
-        appDir.resolve("build.gradle").writeText("""
-            plugins {
-                id('com.android.application')
-                id('kotlin-android')
-                id('com.autonomousapps.dependency-analysis')
-            }
-            android {
-                compileSdkVersion 29
-                defaultConfig {
-                    applicationId "com.autonomousapps.test"
-                    minSdkVersion 21
-                    targetSdkVersion 29
-                    versionCode 1
-                    versionName "1.0"
-                }
-                compileOptions {
-                    sourceCompatibility JavaVersion.VERSION_1_8
-                    targetCompatibility JavaVersion.VERSION_1_8
-                }
-                kotlinOptions {
-                    jvmTarget = "1.8"
-                }
-            }
-            dependencies {
-                implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.50"
-                implementation 'androidx.appcompat:appcompat:1.1.0'
-                implementation 'androidx.core:core-ktx:1.1.0'
-                implementation 'com.google.android.material:material:1.0.0'
-                implementation 'androidx.constraintlayout:constraintlayout:1.1.3'
-                implementation 'androidx.navigation:navigation-fragment-ktx:2.1.0'
-                implementation 'androidx.navigation:navigation-ui-ktx:2.1.0'
-            }
-        """.trimIndent()
-        )
-        val mainDir = appDir.resolve("src/main")
-        mainDir.mkdirs()
-        //android:icon="@mipmap/ic_launcher"
-        //android:roundIcon="@mipmap/ic_launcher_round"
-        mainDir.resolve("AndroidManifest.xml").writeText("""
-            <?xml version="1.0" encoding="utf-8"?>
-            <manifest xmlns:android="http://schemas.android.com/apk/res/android"
-                package="com.autonomousapps.test"
-                >
-            
-                <application
-                    android:allowBackup="true"
-                    android:label="Test app"
-                    android:theme="@style/AppTheme"
-                    >
-                    <activity
-                        android:name=".MainActivity"
-                        android:label="MainActivity"
-                        android:theme="@style/AppTheme.NoActionBar"
-                        >
-                        <intent-filter>
-                            <action android:name="android.intent.action.MAIN" />
-                            <category android:name="android.intent.category.LAUNCHER" />
-                        </intent-filter>
-                    </activity>
-                </application>
-            </manifest>            
-        """.trimIndent()
-        )
-        val resDir = mainDir.resolve("res")
-        resDir.mkdirs()
-
-        val valuesDir = resDir.resolve("values")
-        valuesDir.mkdirs()
-        valuesDir.resolve("styles.xml").writeText("""
-            <?xml version="1.0" encoding="utf-8"?>
-            <resources>
-                <style name="AppTheme" parent="Theme.AppCompat.Light.DarkActionBar">
-                    <item name="colorPrimary">@color/colorPrimary</item>
-                    <item name="colorPrimaryDark">@color/colorPrimaryDark</item>
-                    <item name="colorAccent">@color/colorAccent</item>
-                </style>
-            
-                <style name="AppTheme.NoActionBar">
-                    <item name="windowActionBar">false</item>
-                    <item name="windowNoTitle">true</item>
-                </style>
-            
-                <style name="AppTheme.AppBarOverlay" parent="ThemeOverlay.AppCompat.Dark.ActionBar" />
-            
-                <style name="AppTheme.PopupOverlay" parent="ThemeOverlay.AppCompat.Light" />
-            </resources>
-        """.trimIndent()
-        )
-        valuesDir.resolve("colors.xml").writeText("""
-            <?xml version="1.0" encoding="utf-8"?>
-            <resources>
-                <color name="colorPrimaryDark">#0568ae</color>
-                <color name="colorPrimary">#009fdb</color>
-                <color name="colorAccent">#009fdb</color>
-            </resources>
-        """.trimIndent()
-        )
-        val packageRoot = mainDir.resolve("java/com/autonomousapps/test")
-        packageRoot.mkdirs()
-        packageRoot.resolve("MainActivity.kt").writeText("""
-            package com.autonomousapps.test
-            
-            import androidx.appcompat.app.AppCompatActivity
-            
-            class MainActivity : AppCompatActivity() {
-                
-            }
-        """.trimIndent()
-        )
+        val androidProject = AndroidProject()
 
         // Run the build
-        val runner = GradleRunner.create()
-        runner.forwardOutput()
-        runner.withPluginClasspath()
-        runner.withArguments("app:assembleDebug")
-        runner.withProjectDir(projectDir)
-        val result = runner.build();
+        val result = GradleRunner.create().apply {
+            forwardOutput()
+            withPluginClasspath()
+            withArguments("app:assembleDebug")
+            withProjectDir(androidProject.projectDir)
+        }.build()
 
         // Verify the result
         assertTrue {
@@ -162,4 +27,43 @@ class FunctionalTest {
             result.output.contains("BUILD SUCCESSFUL")
         }
     }
+
+    @Test fun `can execute buildHealth`() {
+        // Setup the test build
+        val androidProject = AndroidProject()
+
+        // Run the build
+        val result = GradleRunner.create().apply {
+            forwardOutput()
+            withPluginClasspath()
+            withArguments("buildHealth")
+            withProjectDir(androidProject.projectDir)
+        }.build()
+
+        // Verify the result
+        assertTrue {
+            // Aggregate tasks
+            result.output.contains("Task :abiReport")
+            result.output.contains("Task :misusedDependenciesReport")
+            result.output.contains("Task :buildHealth")
+
+            // Reports
+            result.hasUnusedDependencies(listOf(
+                "androidx.constraintlayout:constraintlayout",
+                "androidx.core:core-ktx",
+                "androidx.navigation:navigation-fragment-ktx",
+                "androidx.navigation:navigation-ui-ktx",
+                "com.google.android.material:material"
+            ))
+
+            // Result
+            result.output.contains("BUILD SUCCESSFUL")
+        }
+    }
+
+    // TODO the format here is hardcoded. Would be preferable to make it a bit more flexible
+    private fun BuildResult.hasUnusedDependencies(deps: List<String>) = output.contains("""
+        Completely unused dependencies:
+        ${deps.joinToString(prefix = "- ", separator = "\n- ")}
+    """.trimIndent())
 }
---
+++ src/functionalTest/kotlin/com/autonomousapps/utils/AndroidProject.kt
@@ -0,0 +1,165 @@
+package com.autonomousapps.utils
+
+import java.io.File
+
+class AndroidProject {
+
+    val projectDir = File("build/functionalTest")
+
+    init {
+        // Setup the test build
+        projectDir.mkdirs()
+        projectDir.resolve("settings.gradle").writeText("""
+            rootProject.name = 'real-app'
+            include(':app')
+        """.trimIndent())
+        projectDir.resolve("build.gradle").writeText("""
+            buildscript {
+                repositories {
+                    google()
+                    jcenter()
+                }
+                dependencies {
+                    classpath 'com.android.tools.build:gradle:3.5.3'
+                    classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61"
+                }
+            }
+            plugins {
+                id('com.autonomousapps.dependency-analysis')
+            }
+            subprojects {
+                repositories {
+                    google()
+                    jcenter()
+                }
+            }
+        """.trimIndent())
+
+        // https://github.com/gradle/gradle/issues/11338
+        val buildSrcDir = projectDir.resolve("buildSrc")
+        buildSrcDir.mkdirs()
+        buildSrcDir.resolve("settings.gradle").writeText("")
+        buildSrcDir.resolve("build.gradle").writeText("""
+            repositories {
+                jcenter()
+                google()
+                gradlePluginPortal()
+            }
+            dependencies {
+                implementation("com.android.tools.build:gradle:3.5.3")
+                implementation("gradle.plugin.com.autonomousapps:dependency-analysis-gradle-plugin:0.10.0")
+            }
+        """.trimIndent())
+
+        val appDir = projectDir.resolve("app")
+        appDir.mkdirs()
+        appDir.resolve("build.gradle").writeText("""
+            plugins {
+                id('com.android.application')
+                id('kotlin-android')
+            }
+            android {
+                compileSdkVersion 29
+                defaultConfig {
+                    applicationId "com.autonomousapps.test"
+                    minSdkVersion 21
+                    targetSdkVersion 29
+                    versionCode 1
+                    versionName "1.0"
+                }
+                compileOptions {
+                    sourceCompatibility JavaVersion.VERSION_1_8
+                    targetCompatibility JavaVersion.VERSION_1_8
+                }
+                kotlinOptions {
+                    jvmTarget = "1.8"
+                }
+            }
+            dependencies {
+                implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.50"
+                implementation 'androidx.appcompat:appcompat:1.1.0'
+                implementation 'androidx.core:core-ktx:1.1.0'
+                implementation 'com.google.android.material:material:1.0.0'
+                implementation 'androidx.constraintlayout:constraintlayout:1.1.3'
+                implementation 'androidx.navigation:navigation-fragment-ktx:2.1.0'
+                implementation 'androidx.navigation:navigation-ui-ktx:2.1.0'
+            }
+        """.trimIndent()
+        )
+        val mainDir = appDir.resolve("src/main")
+        mainDir.mkdirs()
+        //android:icon="@mipmap/ic_launcher"
+        //android:roundIcon="@mipmap/ic_launcher_round"
+        mainDir.resolve("AndroidManifest.xml").writeText("""
+            <?xml version="1.0" encoding="utf-8"?>
+            <manifest xmlns:android="http://schemas.android.com/apk/res/android"
+                package="com.autonomousapps.test"
+                >
+            
+                <application
+                    android:allowBackup="true"
+                    android:label="Test app"
+                    android:theme="@style/AppTheme"
+                    >
+                    <activity
+                        android:name=".MainActivity"
+                        android:label="MainActivity"
+                        android:theme="@style/AppTheme.NoActionBar"
+                        >
+                        <intent-filter>
+                            <action android:name="android.intent.action.MAIN" />
+                            <category android:name="android.intent.category.LAUNCHER" />
+                        </intent-filter>
+                    </activity>
+                </application>
+            </manifest>            
+        """.trimIndent()
+        )
+        val resDir = mainDir.resolve("res")
+        resDir.mkdirs()
+
+        val valuesDir = resDir.resolve("values")
+        valuesDir.mkdirs()
+        valuesDir.resolve("styles.xml").writeText("""
+            <?xml version="1.0" encoding="utf-8"?>
+            <resources>
+                <style name="AppTheme" parent="Theme.AppCompat.Light.DarkActionBar">
+                    <item name="colorPrimary">@color/colorPrimary</item>
+                    <item name="colorPrimaryDark">@color/colorPrimaryDark</item>
+                    <item name="colorAccent">@color/colorAccent</item>
+                </style>
+            
+                <style name="AppTheme.NoActionBar">
+                    <item name="windowActionBar">false</item>
+                    <item name="windowNoTitle">true</item>
+                </style>
+            
+                <style name="AppTheme.AppBarOverlay" parent="ThemeOverlay.AppCompat.Dark.ActionBar" />
+            
+                <style name="AppTheme.PopupOverlay" parent="ThemeOverlay.AppCompat.Light" />
+            </resources>
+        """.trimIndent()
+        )
+        valuesDir.resolve("colors.xml").writeText("""
+            <?xml version="1.0" encoding="utf-8"?>
+            <resources>
+                <color name="colorPrimaryDark">#0568ae</color>
+                <color name="colorPrimary">#009fdb</color>
+                <color name="colorAccent">#009fdb</color>
+            </resources>
+        """.trimIndent()
+        )
+        val packageRoot = mainDir.resolve("java/com/autonomousapps/test")
+        packageRoot.mkdirs()
+        packageRoot.resolve("MainActivity.kt").writeText("""
+            package com.autonomousapps.test
+            
+            import androidx.appcompat.app.AppCompatActivity
+            
+            class MainActivity : AppCompatActivity() {
+                
+            }
+        """.trimIndent()
+        )
+    }
+}
