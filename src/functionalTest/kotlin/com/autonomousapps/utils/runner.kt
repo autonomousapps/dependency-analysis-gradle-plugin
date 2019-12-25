5f6dd1962948743767879ee7e5555e957e8f890f
Tony Robalik <trobalik@hiya.com>
2019-12-25 00:16:35 -0800

    Cleaned up runner.kt

--- src/functionalTest/kotlin/com/autonomousapps/utils/runner.kt
+++ src/functionalTest/kotlin/com/autonomousapps/utils/runner.kt
@@ -7,21 +7,22 @@ internal fun build(
     gradleVersion: GradleVersion,
     projectDirProvider: ProjectDirProvider,
     vararg args: String
+) = runner(gradleVersion, projectDirProvider, *args).build()
+
+internal fun buildAndFail(
+    gradleVersion: GradleVersion,
+    projectDirProvider: ProjectDirProvider,
+    vararg args: String
+) = runner(gradleVersion, projectDirProvider, *args).buildAndFail()
+
+internal fun runner(
+    gradleVersion: GradleVersion,
+    projectDirProvider: ProjectDirProvider,
+    vararg args: String
 ) = GradleRunner.create().apply {
     forwardOutput()
     withPluginClasspath()
     withGradleVersion(gradleVersion.version)
     withProjectDir(projectDirProvider.projectDir)
     withArguments(*args)
-}.build()
-
-internal fun AndroidProject.build(vararg args: String) = runner(this, *args).build()
-
-internal fun AndroidProject.buildAndFail(vararg args: String) = runner(this, *args).buildAndFail()
-
-internal fun runner(androidProject: AndroidProject, vararg args: String) = GradleRunner.create().apply {
-    forwardOutput()
-    withPluginClasspath()
-    withArguments(*args)
-    withProjectDir(androidProject.projectDir)
 }
