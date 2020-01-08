57421dcc208d3c8630b3d75d7156bf3723a16c1d
Tony Robalik <trobalik@hiya.com>
2020-01-08 14:02:27 -0800

    Added a pre-check to functionalTests.sh.

--- scripts/functionalTests.sh
+++ scripts/functionalTests.sh
@@ -2,7 +2,11 @@
 # Script to execute functional tests against a list of AGP versions
 # Must be run from the project root directory (not THIS directory!)
 
-pwd
+if [[ $(pwd) == *scripts ]]; then
+  >&2 echo "Must execute script from project root"
+  exit 1
+fi
+
 agpVersions=('3.5.3' '3.6.0-rc01' '4.0.0-alpha08')
 
 for v in "${agpVersions[@]}"; do
