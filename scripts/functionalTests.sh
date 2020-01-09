145678f5c3ac79159b1859e55a7292e4f3eac854
Tony Robalik <trobalik@hiya.com>
2020-01-09 11:58:34 -0800

    Updated functional tests script to make it more obvious which AGP versions are failing.

--- scripts/functionalTests.sh
+++ scripts/functionalTests.sh
@@ -2,6 +2,9 @@
 # Script to execute functional tests against a list of AGP versions
 # Must be run from the project root directory (not THIS directory!)
 
+RED='\033[0;31m'
+NC='\033[0m'
+
 if [[ $(pwd) == *scripts ]]; then
   >&2 echo "Must execute script from project root"
   exit 1
@@ -13,4 +16,7 @@ agpVersions=('3.5.3' '3.6.0-rc01')
 for v in "${agpVersions[@]}"; do
   echo "Executing functional tests against AGP $v"
   ./gradlew functionalTest -DfuncTest.agpVersion="$v"
+  if [ $? -ne 0 ]; then
+    >&2 echo -e "${RED}Functional test failed against AGP $v${NC}"
+  fi
 done
