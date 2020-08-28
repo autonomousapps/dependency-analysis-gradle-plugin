63cc072534c085075d8d8f6a5bf8e6a9cd311c65
Tony Robalik <tony.robalik@gmail.com>
2020-08-28 15:01:01 -0700

    Improve support for flat jar/aar files.

--- src/main/kotlin/com/autonomousapps/internal/utils/gradleStrings.kt
+++ src/main/kotlin/com/autonomousapps/internal/utils/gradleStrings.kt
@@ -14,7 +14,13 @@ import org.gradle.internal.component.local.model.OpaqueComponentIdentifier
 internal fun ComponentIdentifier.asString(): String {
   return when (this) {
     is ProjectComponentIdentifier -> projectPath
-    is ModuleComponentIdentifier -> moduleIdentifier.toString()
+    is ModuleComponentIdentifier -> {
+      // flat JAR/AAR files have no group. I don't trust that, if absent, it will be blank rather
+      // than null.
+      @Suppress("UselessCallOnNotNull")
+      if (moduleIdentifier.group.isNullOrBlank()) moduleIdentifier.name
+      else moduleIdentifier.toString()
+    }
     // e.g. "Gradle API"
     is OpaqueComponentIdentifier -> displayName
     // for a file dependency
@@ -26,7 +32,10 @@ internal fun ComponentIdentifier.asString(): String {
 internal fun ComponentIdentifier.resolvedVersion(): String? {
   return when (this) {
     is ProjectComponentIdentifier -> null
-    is ModuleComponentIdentifier -> version
+    is ModuleComponentIdentifier -> {
+      // flat JAR/AAR files have no version, but rather than null, it's empty.
+      if (version.isNotBlank()) version else null
+    }
     // e.g. "Gradle API"
     is OpaqueComponentIdentifier -> null
     // for a file dependency
@@ -37,8 +46,11 @@ internal fun ComponentIdentifier.resolvedVersion(): String? {
 
 internal fun DependencySet.toIdentifiers(): Set<String> = mapNotNullToSet {
   when (it) {
-    is ProjectDependency -> it.dependencyProject.path.intern()
-    is ModuleDependency -> "${it.group}:${it.name}".intern()
+    is ProjectDependency -> it.dependencyProject.path
+    is ModuleDependency -> {
+      // flat JAR/AAR files have no group.
+      if (it.group != null) "${it.group}:${it.name}" else it.name
+    }
     // Don't have enough information, so ignore it
     is SelfResolvingDependency -> null
     else -> throw GradleException("Unknown Dependency subtype: \n$it\n${it.javaClass.name}")
