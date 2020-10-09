d8ede5baaf04d1e24091734ab2cf034c76166a58
Tony Robalik <tony.robalik@gmail.com>
2020-10-09 16:31:04 -0700

    Bumped Gradle Enterprise.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 @file:Suppress("PropertyName")
 
 plugins {
-  id("com.gradle.enterprise") version "3.2.1"
+  id("com.gradle.enterprise") version "3.4.1"
 }
 
 val VERSION: String by extra.properties
