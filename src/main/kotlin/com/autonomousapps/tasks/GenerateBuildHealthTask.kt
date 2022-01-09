autonomousapps/dependency-analysis-gradle-plugin

commit 3e7c37c86dd47a8dae8c52c99e34fff859f7e400

Simplify end of pipeline to improve performance. Eliminate redundant processing.

src/main/kotlin/com/autonomousapps/tasks/GenerateBuildHealthTask.kt
@@ -1,6 +1,7 @@
 package com.autonomousapps.tasks
 
 import com.autonomousapps.TASK_GROUP_DEP_INTERNAL
+import com.autonomousapps.internal.advice.ProjectHealthConsoleReportBuilder
 import com.autonomousapps.internal.utils.fromJson
 import com.autonomousapps.internal.utils.getAndDelete
 import com.autonomousapps.internal.utils.toJson
@@ -26,22 +27,43 @@ abstract class GenerateBuildHealthTask : DefaultTask() {
   @get:OutputFile
   abstract val output: RegularFileProperty
 
+  @get:OutputFile
+  abstract val consoleOutput: RegularFileProperty
+
+  @get:OutputFile
+  abstract val outputFail: RegularFileProperty
+
   @TaskAction fun action() {
     val output = output.getAndDelete()
+    val consoleOutput = consoleOutput.getAndDelete()
+    val outputFail = outputFail.getAndDelete()
+
+    var shouldFail = false
 
     val buildHealth: Set<ProjectAdvice> = projectHealthReports.dependencies.asSequence()
       // They should all be project dependencies, but
       // https://github.com/autonomousapps/dependency-analysis-android-gradle-plugin/issues/295
       .filterIsInstance<ProjectDependency>()
+      // we sort here because of the onEach below, where we stream the console output to disk
+      .sortedBy { it.dependencyProject.path }
       .map { dependency ->
         projectHealthReports.fileCollection(dependency)
           .singleOrNull { it.exists() }
           ?.fromJson<ProjectAdvice>()
-        // There is often no file in the root project, but we'd like it in the report anyway
+          // There is often no file in the root project, but we'd like it in the json report anyway
           ?: ProjectAdvice(dependency.dependencyProject.path)
+      }
+      .onEach { projectAdvice ->
+        if (projectAdvice.isNotEmpty()) {
+          shouldFail = shouldFail || projectAdvice.shouldFail
+
+          val report = ProjectHealthConsoleReportBuilder(projectAdvice).text
+          consoleOutput.appendText("Advice for ${projectAdvice.projectPath}\n$report\n\n")
+        }
       }
       .toSortedSet()
 
     output.writeText(buildHealth.toJson())
+    outputFail.writeText(shouldFail.toString())
   }
 }

src/main/kotlin/com/autonomousapps/internal/OutputPaths.kt
@@ -125,6 +125,7 @@ internal class RootOutputPaths(private val project: Project) {
   // v2
   val buildHealthPath = file("$ROOT_DIR/build-health-report.json")
   val consoleReportPath = file("$ROOT_DIR/build-health-report.txt")
+  val shouldFailPath = file("$ROOT_DIR/should-fail.txt")
 
   // v1
   val strictAdvicePath = file("$ROOT_DIR/advice-holistic-strict.json")

src/main/kotlin/com/autonomousapps/subplugin/RootPlugin.kt
@@ -85,18 +85,22 @@ internal class RootPlugin(private val project: Project) {
 
     val generateBuildHealthTask = tasks.register<GenerateBuildHealthTask>("generateBuildHealth") {
       dependsOn(adviceAllConf)
-
       projectHealthReports = adviceAllConf
+
       output.set(paths.buildHealthPath)
+      consoleOutput.set(paths.consoleReportPath)
+      outputFail.set(paths.shouldFailPath)
     }
 
-    val generateBuildHealthReportTask = tasks.register<GenerateBuildHealthReportTask>("generateBuildHealthReport") {
-      buildHealth.set(generateBuildHealthTask.flatMap { it.output })
-      output.set(paths.consoleReportPath)
-    }
+    // val generateBuildHealthReportTask = tasks.register<GenerateBuildHealthReportTask>("generateBuildHealthReport") {
+    //   buildHealth.set(generateBuildHealthTask.flatMap { it.output })
+    //   output.set(paths.consoleReportPath)
+    // }
 
     tasks.register<BuildHealthTask2>("buildHealth") {
-      consoleReport.set(generateBuildHealthReportTask.flatMap { it.output })
+      //consoleReport.set(generateBuildHealthReportTask.flatMap { it.output })
+      shouldFail.set(generateBuildHealthTask.flatMap { it.outputFail })
+      consoleReport.set(generateBuildHealthTask.flatMap { it.consoleOutput })
       buildHealth.set(generateBuildHealthTask.flatMap { it.output })
     }
   }

src/main/kotlin/com/autonomousapps/tasks/BuildHealthTask2.kt
@@ -20,6 +20,10 @@ abstract class BuildHealthTask2 : DefaultTask() {
 
   @get:PathSensitive(PathSensitivity.NONE)
   @get:InputFile
+  abstract val shouldFail: RegularFileProperty
+
+  @get:PathSensitive(PathSensitivity.NONE)
+  @get:InputFile
   abstract val consoleReport: RegularFileProperty
 
   @get:PathSensitive(PathSensitivity.NONE)
@@ -27,17 +31,17 @@ abstract class BuildHealthTask2 : DefaultTask() {
   abstract val buildHealth: RegularFileProperty
 
   @TaskAction fun action() {
+    val shouldFail = shouldFail.get().asFile.readText().toBoolean()
     val consoleReportFile = consoleReport.get().asFile
-    val consoleReportText = consoleReport.get().asFile.readText()
     val consoleReportPath = consoleReportFile.absolutePath
-    val buildHealth = buildHealth.fromJsonSet<ProjectAdvice>()
+    val hasAdvice = consoleReportFile.length() > 0
 
     val output = "See report at $consoleReportPath"
 
-    if (buildHealth.any { it.shouldFail }) {
-      check(consoleReportText.isNotBlank()) { "Console report should not be blank if buildHealth should fail" }
+    if (shouldFail) {
+      check(hasAdvice) { "Console report should not be blank if buildHealth should fail" }
       throw BuildHealthException(output)
-    } else if (consoleReportText.isNotBlank()) {
+    } else if (hasAdvice) {
       logger.quiet(output)
     }
   }

commit 35d38bd1d1c66edefe84fac0f431172ecfb30da1

Always create build-health-report.txt. Fix issue introduced in  3e7c37c.

src/main/kotlin/com/autonomousapps/tasks/GenerateBuildHealthTask.kt
@@ -38,6 +38,7 @@ abstract class GenerateBuildHealthTask : DefaultTask() {
     val consoleOutput = consoleOutput.getAndDelete()
     val outputFail = outputFail.getAndDelete()
 
+    var didWrite = false
     var shouldFail = false
 
     val buildHealth: Set<ProjectAdvice> = projectHealthReports.dependencies.asSequence()
@@ -59,11 +60,16 @@ abstract class GenerateBuildHealthTask : DefaultTask() {
 
           val report = ProjectHealthConsoleReportBuilder(projectAdvice).text
           consoleOutput.appendText("Advice for ${projectAdvice.projectPath}\n$report\n\n")
+          didWrite = true
         }
       }
       .toSortedSet()
 
     output.writeText(buildHealth.toJson())
     outputFail.writeText(shouldFail.toString())
+    if (!didWrite) {
+      // This file must always exist, even if empty
+      consoleOutput.writeText("")
+    }
   }
 }