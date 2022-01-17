fb041c02b0b7e545797e99e992fb301936dea8c4
Tony Robalik <tony.robalik@gmail.com>
2022-01-17 01:00:01 -0800

    Delete old, redundant specs.

--- src/functionalTest/groovy/com/autonomousapps/jvm/PostProcessingSpec.groovy
+++ src/functionalTest/groovy/com/autonomousapps/jvm/PostProcessingSpec.groovy
@@ -1,61 +1,13 @@
 package com.autonomousapps.jvm
 
-import com.autonomousapps.fixtures.PostProcessingProject
-import com.autonomousapps.fixtures.ProjectDirProvider
-import com.autonomousapps.jvm.projects.PostProcessingProject2
 import com.autonomousapps.jvm.projects.PostProcessingProject3
 import org.gradle.testkit.runner.TaskOutcome
 import org.gradle.util.GradleVersion
-import org.spockframework.runtime.extension.builtin.PreconditionContext
-import spock.lang.IgnoreIf
 
 import static com.autonomousapps.utils.Runner.build
 import static com.google.common.truth.Truth.assertThat
 
 final class PostProcessingSpec extends AbstractJvmSpec {
-
-  private ProjectDirProvider javaLibraryProject = null
-
-  def cleanup() {
-    if (javaLibraryProject != null) {
-      clean(javaLibraryProject)
-    }
-  }
-
-  // TODO V2: Delete. The new spec is better.
-  @IgnoreIf({ PreconditionContext it -> it.sys.v == '2' })
-  def "can post-process root project output (#gradleVersion)"() {
-    given:
-    javaLibraryProject = new PostProcessingProject()
-
-    when:
-    def result = build(gradleVersion, javaLibraryProject, ':postProcess')
-
-    then: 'The advice task executes (task dependencies work)'
-    result.task(':generateAdviceMain').outcome == TaskOutcome.SUCCESS
-    result.task(':postProcess').outcome == TaskOutcome.SUCCESS
-
-    where:
-    gradleVersion << gradleVersions()
-  }
-
-  // TODO V2: Delete. The new spec is better.
-  @IgnoreIf({ PreconditionContext it -> it.sys.v == '2' })
-  def "can post-process subproject output (#gradleVersion)"() {
-    given:
-    def project = new PostProcessingProject2()
-    gradleProject = project.gradleProject
-
-    when:
-    def result = build(gradleVersion, gradleProject.rootDir, ':proj-1:postProcess')
-
-    then: 'The advice task executes (task dependencies work)'
-    result.task(':proj-1:generateAdviceMain').outcome == TaskOutcome.SUCCESS
-    result.task(':proj-1:postProcess').outcome == TaskOutcome.SUCCESS
-
-    where:
-    gradleVersion << gradleVersions()
-  }
 
   def "can post-process advice with abstract task (#gradleVersion isV1=#isV1)"() {
     given:
