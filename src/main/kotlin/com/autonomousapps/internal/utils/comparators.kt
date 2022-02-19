4e873b94b26990dbbc294eb884b4f3030bcb5de5
Vladimir Mironov <nsk.mironov@gmail.com>
2022-02-20 01:02:21 +0300

    Make LexicographicIterableComparator implementation a bit less verbose.

--- src/main/kotlin/com/autonomousapps/internal/utils/comparators.kt
+++ src/main/kotlin/com/autonomousapps/internal/utils/comparators.kt
@@ -2,30 +2,22 @@ package com.autonomousapps.internal.utils
 
 internal class LexicographicIterableComparator<T : Comparable<T>> : Comparator<Iterable<T>> {
   override fun compare(left: Iterable<T>?, right: Iterable<T>?): Int {
-    if (left === right) {
-      return 0
-    }
-
-    if (left == null || right == null) {
-      return if (left == null) -1 else 1
-    }
+    if (left === right) return 0
+    if (left == null || right == null) return if (left == null) -1 else 1
 
     val leftIterator = left.iterator()
     val rightIterator = right.iterator()
 
     while (leftIterator.hasNext() && rightIterator.hasNext()) {
-      val leftElement = leftIterator.next()
-      val rightElement = rightIterator.next()
-      val compareResult = leftElement.compareTo(rightElement)
-
+      val compareResult = leftIterator.next().compareTo(rightIterator.next())
       if (compareResult != 0) {
         return compareResult
       }
     }
 
-    val leftHasNext = if (leftIterator.hasNext()) 1 else 0
-    val rightHasNext = if (rightIterator.hasNext()) 1 else 0
+    if (leftIterator.hasNext()) return 1
+    if (rightIterator.hasNext()) return -1
 
-    return leftHasNext - rightHasNext
+    return 0
   }
 }
