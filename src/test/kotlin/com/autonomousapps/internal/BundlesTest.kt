ae59bc4636041703d557f10572ac6e490b135bf5
Tony Robalik <tony.robalik@gmail.com>
2022-05-09 20:43:52 -0700

    Add another bundle test.

--- src/test/kotlin/com/autonomousapps/internal/BundlesTest.kt
+++ src/test/kotlin/com/autonomousapps/internal/BundlesTest.kt
@@ -9,6 +9,7 @@ import com.autonomousapps.model.intermediates.Usage
 import com.autonomousapps.test.usage
 import com.google.common.truth.Truth.assertThat
 import org.gradle.testfixtures.ProjectBuilder
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 @Suppress("UnstableApiUsage")
@@ -18,7 +19,7 @@ class BundlesTest {
   private val objects = project.objects
   private val dependenciesHandler = DependenciesHandler(objects)
 
-  @Test fun `kotlin stdlib as a default bundle`() {
+  @Test fun `kotlin stdlib is a default bundle`() {
     val consumer = ProjectCoordinates(":consumer")
     val stdlibJdk8 = ModuleCoordinates("org.jetbrains.kotlin:kotlin-stdlib-jdk8", "1")
     val stdlib = ModuleCoordinates("org.jetbrains.kotlin:kotlin-stdlib", "1")
@@ -29,11 +30,9 @@ class BundlesTest {
     val dependencyUsages: Map<Coordinates, Set<Usage>> = listOf(stdlibJdk8Usages, stdlibUsages).toMap()
 
     // Dependency graph rooted on :consumer
-    val graph = mapOf(
-      "main,Main" to newGraphFrom(
-        // :consumer -> stdlib-jdk8 -> stdlib
-        listOf(consumer to stdlibJdk8, stdlibJdk8 to stdlib)
-      )
+    val graph = newGraphFrom(
+      // :consumer -> stdlib-jdk8 -> stdlib
+      listOf(consumer to stdlibJdk8, stdlibJdk8 to stdlib)
     )
 
     // the thing under test
@@ -49,62 +48,78 @@ class BundlesTest {
     assertThat(bundles.hasUsedChild(stdlib)).isFalse()
   }
 
-  @Test fun `supports facades with a primary entry point`() {
-    // Define a bundle with a primary identifier
-    dependenciesHandler.bundles.create("facade").apply {
-      primary(":entry-point")
-      includeDependency(":entry-point")
-      includeDependency(":used")
+  @Nested inner class Primary {
+    @Test fun `supports facades with a primary entry point`() {
+      // Define a bundle with a primary identifier
+      dependenciesHandler.bundles.create("facade").apply {
+        primary(":entry-point")
+        includeDependency(":entry-point")
+        includeDependency(":used")
+      }
+      val bundles = buildBundles()
+
+      val badAdvice = Advice.ofAdd(ProjectCoordinates(":used"), "implementation")
+      val expectedAdvice = Advice.ofAdd(ProjectCoordinates(":entry-point"), "implementation")
+      assertThat(bundles.primary(badAdvice)).isEqualTo(expectedAdvice)
     }
 
-    // Coordinates
-    val consumer = ProjectCoordinates(":consumer")
-    val unused = ProjectCoordinates(":unused")
-    val entryPoint = ProjectCoordinates(":entry-point")
-    val used = ProjectCoordinates(":used")
+    @Test fun `without a primary, bundle ignored`() {
+      // Define a bundle without a primary identifier
+      dependenciesHandler.bundles.create("facade").apply {
+        includeDependency(":entry-point")
+        includeDependency(":used")
+      }
+      val bundles = buildBundles()
 
-    // Usages of project :consumer
-    val unusedUsages = unused to usage(Bucket.NONE, "main").intoSet()
-    val entryPointUsages = entryPoint to usage(Bucket.NONE, "main").intoSet()
-    val usedUsages = used to usage(Bucket.IMPL, "main").intoSet()
-    val dependencyUsages: Map<Coordinates, Set<Usage>> = listOf(unusedUsages, entryPointUsages, usedUsages).toMap()
+      // Advice is unchanged
+      val advice = Advice.ofAdd(ProjectCoordinates(":used"), "implementation")
+      assertThat(bundles.primary(advice)).isEqualTo(advice)
+    }
 
-    // Dependency graph rooted on :consumer
-    val graph = mapOf(
-      "main,Main" to newGraphFrom(
+    private fun buildBundles(): Bundles {
+      // Coordinates
+      val consumer = ProjectCoordinates(":consumer")
+      val unused = ProjectCoordinates(":unused")
+      val entryPoint = ProjectCoordinates(":entry-point")
+      val used = ProjectCoordinates(":used")
+
+      // Usages of project :consumer
+      val unusedUsages = unused to usage(Bucket.NONE, "main").intoSet()
+      val entryPointUsages = entryPoint to usage(Bucket.NONE, "main").intoSet()
+      val usedUsages = used to usage(Bucket.IMPL, "main").intoSet()
+      val dependencyUsages: Map<Coordinates, Set<Usage>> = listOf(unusedUsages, entryPointUsages, usedUsages).toMap()
+
+      // Dependency graph rooted on :consumer
+      val graph = newGraphFrom(
         // :consumer -> :unused -> :entry-point -> :used
         listOf(consumer to unused, unused to entryPoint, entryPoint to used)
       )
-    )
 
-    // the thing under test
-    val bundles = Bundles.of(
-      projectNode = ProjectCoordinates(":consumer"),
-      dependencyGraph = graph,
-      bundleRules = dependenciesHandler.serializableBundles(),
-      dependencyUsages = dependencyUsages,
-      ignoreKtx = false
-    )
-
-    val badAdvice = Advice.ofAdd(ProjectCoordinates(":used"), "implementation")
-    val expectedAdvice = Advice.ofAdd(ProjectCoordinates(":entry-point"), "implementation")
-
-    assertThat(bundles.primary(badAdvice)).isEqualTo(expectedAdvice)
+      return Bundles.of(
+        projectNode = ProjectCoordinates(":consumer"),
+        dependencyGraph = graph,
+        bundleRules = dependenciesHandler.serializableBundles(),
+        dependencyUsages = dependencyUsages,
+        ignoreKtx = false
+      )
+    }
   }
 
   private fun newGraphFrom(
     edges: List<Pair<Coordinates, Coordinates>>,
     variant: Variant = Variant.MAIN,
     configurationName: String = "compileClasspath"
-  ): DependencyGraphView {
+  ): Map<String, DependencyGraphView> {
     val graph = DependencyGraphView.newGraphBuilder().apply {
       edges.forEach { putEdge(it.first, it.second) }
     }.build()
 
-    return DependencyGraphView(
-      variant = variant,
-      configurationName = configurationName,
-      graph = graph
+    return mapOf(
+      "main,Main" to DependencyGraphView(
+        variant = variant,
+        configurationName = configurationName,
+        graph = graph
+      )
     )
   }
 }
