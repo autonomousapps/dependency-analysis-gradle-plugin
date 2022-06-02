f270d74a6357fb8c0b128f887e3608dd05cd4f87
Jendrik Johannes <jendrik.johannes@gmail.com>
2022-06-02 12:59:19 +0200

    Add another test case with a simpler project setup

--- src/functionalTest/groovy/com/autonomousapps/jvm/ComponentWithMultipleArtifactsSpec.groovy
+++ src/functionalTest/groovy/com/autonomousapps/jvm/ComponentWithMultipleArtifactsSpec.groovy
@@ -1,6 +1,7 @@
 package com.autonomousapps.jvm
 
 import com.autonomousapps.jvm.projects.JarTransformingProject
+import com.autonomousapps.jvm.projects.MultipleJarsProject
 
 import static com.autonomousapps.utils.Runner.build
 import static com.google.common.truth.Truth.assertThat
@@ -8,6 +9,21 @@ import static com.google.common.truth.Truth.assertThat
 final class ComponentWithMultipleArtifactsSpec extends AbstractJvmSpec {
 
   def "one component can have multiple Jars (#gradleVersion)"() {
+    given:
+    def project = new MultipleJarsProject()
+    gradleProject = project.gradleProject
+
+    when:
+    build(gradleVersion, gradleProject.rootDir, 'buildHealth')
+
+    then:
+    assertThat(project.actualBuildHealth()).containsExactlyElementsIn(project.expectedBuildHealth)
+
+    where:
+    gradleVersion << gradleVersions()
+  }
+
+  def "one component can have multiple Jars produced by a transform (#gradleVersion)"() {
     given:
     def project = new JarTransformingProject()
     gradleProject = project.gradleProject
---
+++ src/functionalTest/groovy/com/autonomousapps/jvm/projects/MultipleJarsProject.groovy
@@ -0,0 +1,95 @@
+package com.autonomousapps.jvm.projects
+
+import com.autonomousapps.AbstractProject
+import com.autonomousapps.kit.GradleProject
+import com.autonomousapps.kit.Plugin
+import com.autonomousapps.kit.Source
+import com.autonomousapps.kit.SourceType
+import com.autonomousapps.model.Advice
+import com.autonomousapps.model.ProjectAdvice
+
+import static com.autonomousapps.AdviceHelper.*
+import static com.autonomousapps.kit.Dependency.project
+
+final class MultipleJarsProject extends AbstractProject {
+
+  final GradleProject gradleProject
+
+  MultipleJarsProject() {
+    this.gradleProject = build()
+  }
+
+  private GradleProject build() {
+    def builder = newGradleProjectBuilder()
+    builder.withSubproject('producer') { s ->
+      s.sources = producerSources
+      s.withBuildScript { bs ->
+        bs.plugins = [Plugin.javaLibraryPlugin]
+        bs.additions = '''
+          def extraJar = tasks.register("extraJar", Jar) {
+            archiveClassifier = 'extra'
+          }
+          configurations.apiElements.outgoing {
+            // make sure the extraJar (which is empty) turns up first
+            artifacts.clear()
+            artifact(extraJar)
+            artifact(tasks.jar)
+          }
+        '''
+      }
+    }
+    builder.withSubproject('consumer') { s ->
+      s.sources = consumerSources
+      s.withBuildScript { bs ->
+        bs.plugins = [Plugin.javaLibraryPlugin]
+        bs.dependencies = [
+          project('implementation', ':producer')
+        ]
+      }
+    }
+
+    def project = builder.build()
+    project.writer().write()
+    return project
+  }
+
+  private producerSources = [
+    new Source(
+      SourceType.JAVA, "ExampleProducer", "com/example/producer",
+      """\
+        package com.example.producer;
+        
+        public class ExampleProducer {
+        }
+      """.stripIndent()
+    )
+  ]
+
+  private consumerSources = [
+    new Source(
+      SourceType.JAVA, "ExampleConsumer", "com/example/consumer",
+      """\
+        package com.example.consumer;
+        
+        import com.example.producer.ExampleProducer;
+        
+        public class ExampleConsumer {
+          public ExampleProducer producer;
+        }
+      """.stripIndent()
+    )
+  ]
+
+  Set<ProjectAdvice> actualBuildHealth() {
+    return actualProjectAdvice(gradleProject)
+  }
+
+  private final Set<Advice> producerProjAdvice = [
+    Advice.ofChange(projectCoordinates(':producer'), 'implementation', 'api')
+  ]
+
+  final Set<ProjectAdvice> expectedBuildHealth = [
+    projectAdviceForDependencies(':consumer', producerProjAdvice),
+    emptyProjectAdviceFor(':producer')
+  ]
+}
