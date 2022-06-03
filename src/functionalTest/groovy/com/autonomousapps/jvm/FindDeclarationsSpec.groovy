c2248fdb060df69750f23babb6accb7951f74b83
Tony Robalik <tony.robalik@gmail.com>
2022-06-03 15:56:43 -0700

    Add spec verifying that FindDeclarationsTask respects changing inputs.

---
+++ src/functionalTest/groovy/com/autonomousapps/jvm/FindDeclarationsSpec.groovy
@@ -0,0 +1,33 @@
+package com.autonomousapps.jvm
+
+import com.autonomousapps.jvm.projects.FindDeclarationsProject
+
+import static com.autonomousapps.kit.truth.BuildTaskSubject.buildTasks
+import static com.autonomousapps.utils.Runner.build
+import static com.google.common.truth.Truth.assertAbout
+
+final class FindDeclarationsSpec extends AbstractJvmSpec {
+
+  def "findDeclarations is not up to date when build script changes (#gradleVersion)"() {
+    given:
+    def project = new FindDeclarationsProject()
+    gradleProject = project.gradleProject
+    def task = ":${project.name}:findDeclarations"
+
+    when: 'We build the first time'
+    def result = build(gradleVersion, gradleProject.rootDir, task)
+
+    then: 'Task was successful'
+    assertAbout(buildTasks()).that(result.task(task)).succeeded()
+
+    when: 'We mutate the build script and build again'
+    project.mutateBuildScript()
+    result = build(gradleVersion, gradleProject.rootDir, task)
+
+    then: 'Task was not up to date'
+    assertAbout(buildTasks()).that(result.task(task)).succeeded()
+
+    where:
+    gradleVersion << gradleVersions()
+  }
+}
---
+++ src/functionalTest/groovy/com/autonomousapps/jvm/projects/FindDeclarationsProject.groovy
@@ -0,0 +1,49 @@
+package com.autonomousapps.jvm.projects
+
+import com.autonomousapps.AbstractProject
+import com.autonomousapps.kit.GradleProject
+import com.autonomousapps.kit.Plugin
+
+import java.nio.file.Files
+
+import static com.autonomousapps.kit.Dependency.okHttp
+import static com.autonomousapps.kit.Dependency.okio
+
+class FindDeclarationsProject extends AbstractProject {
+
+  final name = 'proj'
+  final GradleProject gradleProject
+
+  FindDeclarationsProject() {
+    this.gradleProject = build()
+  }
+
+  private GradleProject build() {
+    def builder = newGradleProjectBuilder()
+    builder.withSubproject(name) { s ->
+      s.withBuildScript { bs ->
+        bs.plugins = [Plugin.javaLibraryPlugin]
+        bs.dependencies = [
+          okHttp('implementation'),
+          okio('implementation'),
+        ]
+      }
+    }
+
+    def project = builder.build()
+    project.writer().write()
+    return project
+  }
+
+  void mutateBuildScript() {
+    def f = gradleProject.rootDir.toPath().resolve("$name/build.gradle")
+    assert Files.exists(f)
+
+    def lines = f.readLines()
+    lines.removeIf {
+      it.contains('okio')
+    }
+
+    f.write(lines.join('\n'))
+  }
+}
