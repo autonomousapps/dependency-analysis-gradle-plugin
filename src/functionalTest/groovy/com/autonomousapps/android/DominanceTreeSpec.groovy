ed7a27dd75f39f4ae45bad762e43a14d6ff78398
Tony Robalik <tony.robalik@gmail.com>
2022-06-12 11:13:01 -0700

    Working on fuzzy matcher for float values.

--- src/functionalTest/groovy/com/autonomousapps/android/DominanceTreeSpec.groovy
+++ src/functionalTest/groovy/com/autonomousapps/android/DominanceTreeSpec.groovy
@@ -11,17 +11,39 @@ import static com.google.common.truth.Truth.assertThat
 final class DominanceTreeSpec extends AbstractAndroidSpec {
 
   // regex: ^.*(\d+\.?\d*)
-//  private static FLOAT = ~/[^:]?(\d+[.]?\d+)/
+  private static FLOAT = ~/[^:\d]?(\d+[.]?\d+)/
 
   //missing (2)   : 4364.27 KiB :app, \--- 1.73 KiB :lib
   //unexpected (2): 4363.98 KiB :app, \--- 1.44 KiB :lib
   private static BinaryPredicate P = { String actual, String expected ->
-    // TODO regex is a huge pain in the ass
 //    def actualMatcher = actual =~ FLOAT
-//    println actualMatcher.findAll()
+//    actualMatcher.findAll()
     actual == expected
   }
   private static Correspondence fuzzyMatcher = Correspondence.from(P, "Fuzzy matcher")
+
+  def "regex works"() {
+    given:
+    def app = "4364.27 KiB :app"
+    def lib = "\\--- 1.73 KiB :lib"
+    def lib2 = "\\--- 1.73 KiB (2.00 KiB) :lib"
+
+    when:
+    def appMatcher = app =~ FLOAT
+    def libMatcher = lib =~ FLOAT
+    def lib2Matcher = lib2 =~ FLOAT
+
+    then:
+    appMatcher.findAll()*.get(1) == ['4364.27']
+    libMatcher.findAll()*.get(1) == ['1.73']
+    lib2Matcher.findAll()*.get(1) == ['1.73', '2.00']
+
+    and:
+    Float.parseFloat(appMatcher.findAll()*.get(1)[0] as String) == 4364.27f
+    Float.parseFloat(libMatcher.findAll()*.get(1)[0] as String) == 1.73f
+    Float.parseFloat(lib2Matcher.findAll()*.get(1)[0] as String) == 1.73f
+    Float.parseFloat(lib2Matcher.findAll()*.get(1)[1] as String) == 2.00f
+  }
 
   def "can print dominance tree (#gradleVersion AGP #agpVersion)"() {
     given:
