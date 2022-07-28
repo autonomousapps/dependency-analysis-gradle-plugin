7889546256a603501b8cb06285ebe4edc68a0efb
Tony Robalik <tony.robalik@gmail.com>
2022-07-28 14:23:58 -0700

    Test against Gradle 7.5, not 7.5-rc-2.

--- src/functionalTest/groovy/com/autonomousapps/AbstractFunctionalSpec.groovy
+++ src/functionalTest/groovy/com/autonomousapps/AbstractFunctionalSpec.groovy
@@ -17,7 +17,7 @@ abstract class AbstractFunctionalSpec extends Specification {
   protected static final GRADLE_7_2 = GradleVersion.version('7.2')
   protected static final GRADLE_7_3 = GradleVersion.version('7.3.3')
   protected static final GRADLE_7_4 = GradleVersion.version('7.4.2')
-  protected static final GRADLE_7_5 = GradleVersion.version('7.5-rc-2')
+  protected static final GRADLE_7_5 = GradleVersion.version('7.5')
 
   // For faster CI times, we only test min + max. Testing all would be preferable, but we don't have till the heat death
   // of the universe to wait.
