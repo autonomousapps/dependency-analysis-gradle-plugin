9f56e2785099db10b6cd210c2601b546111fed6c
Jendrik Johannes <jendrik.johannes@gmail.com>
2022-11-15 12:16:32 +0100

    Add test cases for depending on included build projects

--- src/functionalTest/groovy/com/autonomousapps/jvm/IncludedBuildSpec.groovy
+++ src/functionalTest/groovy/com/autonomousapps/jvm/IncludedBuildSpec.groovy
@@ -1,12 +1,17 @@
 package com.autonomousapps.jvm
 
 import com.autonomousapps.jvm.projects.IncludedBuildProject
+import com.autonomousapps.jvm.projects.IncludedBuildWithSubprojectsProject
 
 import static com.autonomousapps.utils.Runner.build
 import static com.google.common.truth.Truth.assertThat
 
 // https://github.com/autonomousapps/dependency-analysis-android-gradle-plugin/issues/565
 final class IncludedBuildSpec extends AbstractJvmSpec {
+
+  private static INCLUDED_BUILD_SUPPORT_GRADLE_VERSIONS = [
+    GRADLE_7_3, GRADLE_7_4, SUPPORTED_GRADLE_VERSIONS.last()
+  ]
 
   def "doesn't crash in presence of an included build (#gradleVersion)"() {
     given:
@@ -20,6 +25,51 @@ final class IncludedBuildSpec extends AbstractJvmSpec {
     assertThat(project.actualBuildHealth()).containsExactlyElementsIn(project.expectedBuildHealth)
 
     where: 'This new feature only works for Gradle 7.3+'
-    gradleVersion << [GRADLE_7_3, GRADLE_7_4]
+    gradleVersion << INCLUDED_BUILD_SUPPORT_GRADLE_VERSIONS
+  }
+
+  def "does not confuse identities of included subprojects depended on by another build"() {
+    given:
+    def project = new IncludedBuildWithSubprojectsProject()
+    gradleProject = project.gradleProject
+
+    when: 'build does not fail'
+    build(gradleVersion, gradleProject.rootDir, ':buildHealth')
+
+    then: 'and there is no advice'
+    assertThat(project.actualBuildHealth()).containsExactlyElementsIn(project.expectedBuildHealth)
+
+    where:
+    gradleVersion << INCLUDED_BUILD_SUPPORT_GRADLE_VERSIONS
+  }
+
+  def "does not confuse identities of included subprojects depending on each other by GA dependency notation"() {
+    given:
+    def project = new IncludedBuildWithSubprojectsProject()
+    gradleProject = project.gradleProject
+
+    when: 'build does not fail'
+    build(gradleVersion, gradleProject.rootDir, ':second-build:buildHealth')
+
+    then: 'and there is no advice'
+    assertThat(project.actualIncludedBuildHealth()).containsExactlyElementsIn(project.expectedIncludedBuildHealth)
+
+    where:
+    gradleVersion << INCLUDED_BUILD_SUPPORT_GRADLE_VERSIONS
+  }
+
+  def "does not confuse identities of included subprojects depending on each other by project dependency notation"() {
+    given:
+    def project = new IncludedBuildWithSubprojectsProject(true)
+    gradleProject = project.gradleProject
+
+    when: 'build does not fail'
+    build(gradleVersion, gradleProject.rootDir, ':second-build:buildHealth')
+
+    then: 'and there is no advice'
+    assertThat(project.actualIncludedBuildHealth()).containsExactlyElementsIn(project.expectedIncludedBuildHealth)
+
+    where:
+    gradleVersion << INCLUDED_BUILD_SUPPORT_GRADLE_VERSIONS
   }
 }
