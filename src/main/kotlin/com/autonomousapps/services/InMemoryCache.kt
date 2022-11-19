4284f5cc256f3a9d4ea1f5164d50fb1c9efe88e3
Tony Robalik <tony.robalik@gmail.com>
2022-11-18 16:18:49 -0800

    Eliminate some redundant work relating to JarExploder.

--- src/main/kotlin/com/autonomousapps/services/InMemoryCache.kt
+++ src/main/kotlin/com/autonomousapps/services/InMemoryCache.kt
@@ -13,7 +13,6 @@ import org.gradle.api.provider.Property
 import org.gradle.api.provider.Provider
 import org.gradle.api.services.BuildService
 import org.gradle.api.services.BuildServiceParameters
-import java.util.concurrent.ConcurrentSkipListMap
 
 abstract class InMemoryCache : BuildService<InMemoryCache.Params> {
 
@@ -22,22 +21,6 @@ abstract class InMemoryCache : BuildService<InMemoryCache.Params> {
   }
 
   private val cacheSize = parameters.cacheSize.get()
-  private val jars: MutableMap<String, Int> = ConcurrentSkipListMap()
-  private val classes: MutableMap<String, Int> = ConcurrentSkipListMap()
-
-  internal fun updateJars(jarName: String) {
-    jars.merge(jarName, 1) { oldValue, increment -> oldValue + increment }
-  }
-
-  internal fun updateClasses(className: String) {
-    classes.merge(className, 1) { oldValue, increment -> oldValue + increment }
-  }
-
-  internal fun classes(): Map<String, Int> = classes
-
-  /*
-   * Caches.
-   */
 
   private inline fun <reified K, reified V> newCache(maxSize: Long = cacheSize): Cache<K, V> {
     val builder = Caffeine.newBuilder()
@@ -46,23 +29,23 @@ abstract class InMemoryCache : BuildService<InMemoryCache.Params> {
   }
 
   private val explodingJars: Cache<String, ExplodingJar> = newCache()
-  private val inlineMembers2: Cache<String, Set<InlineMemberCapability.InlineMember>> = newCache()
-  private val procs2: Cache<String, AnnotationProcessorDependency> = newCache()
+  private val inlineMembers: Cache<String, Set<InlineMemberCapability.InlineMember>> = newCache()
+  private val procs: Cache<String, AnnotationProcessorDependency> = newCache()
 
   internal fun explodedJar(name: String): ExplodingJar? = explodingJars.asMap()[name]
   internal fun explodedJars(name: String, explodingJar: ExplodingJar) {
     explodingJars.asMap().putIfAbsent(name, explodingJar)
   }
 
-  internal fun inlineMember(name: String): Set<InlineMemberCapability.InlineMember>? = inlineMembers2.asMap()[name]
+  internal fun inlineMember(name: String): Set<InlineMemberCapability.InlineMember>? = inlineMembers.asMap()[name]
 
   internal fun inlineMembers(name: String, members: Set<InlineMemberCapability.InlineMember>) {
-    inlineMembers2.asMap().putIfAbsent(name, members)
+    inlineMembers.asMap().putIfAbsent(name, members)
   }
 
-  internal fun proc(procName: String): AnnotationProcessorDependency? = procs2.asMap()[procName]
+  internal fun proc(procName: String): AnnotationProcessorDependency? = procs.asMap()[procName]
   internal fun procs(procName: String, proc: AnnotationProcessorDependency) {
-    procs2.asMap().putIfAbsent(procName, proc)
+    procs.asMap().putIfAbsent(procName, proc)
   }
 
   companion object {
