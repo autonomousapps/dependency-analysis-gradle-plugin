cd4cd88f4ba829d442ac3f7c7f6ab903e2be9687
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-01-31 17:48:03 +0100

    Print addressing a feature variant in a dependency in advices

--- src/main/kotlin/com/autonomousapps/internal/advice/AdvicePrinter.kt
+++ src/main/kotlin/com/autonomousapps/internal/advice/AdvicePrinter.kt
@@ -2,6 +2,7 @@ package com.autonomousapps.internal.advice
 
 import com.autonomousapps.model.Advice
 import com.autonomousapps.model.Coordinates
+import com.autonomousapps.model.ModuleCoordinates
 import com.autonomousapps.model.ProjectCoordinates
 
 internal class AdvicePrinter(
@@ -10,21 +11,41 @@ internal class AdvicePrinter(
   private val dependencyMap: (String) -> String = { it },
 ) {
 
-  fun line(configuration: String, printableIdentifier: String, was: String = ""): String = when (dslKind) {
-    DslKind.KOTLIN -> "  $configuration($printableIdentifier)$was"
-    DslKind.GROOVY -> "  $configuration $printableIdentifier$was"
-  }
+  fun line(configuration: String, printableIdentifier: String, was: String = ""): String =
+    "  $configuration$printableIdentifier$was"
 
-  fun toDeclaration(advice: Advice): String = when (dslKind) {
-    DslKind.KOTLIN -> "  ${advice.toConfiguration}(${gav(advice.coordinates)})"
-    DslKind.GROOVY -> "  ${advice.toConfiguration} ${gav(advice.coordinates)}"
-  }
+  fun toDeclaration(advice: Advice): String =
+    "  ${advice.toConfiguration}${gav(advice.coordinates)}"
 
   fun gav(coordinates: Coordinates): String {
     val quotedDep = coordinates.mapped()
     return when (coordinates) {
       is ProjectCoordinates -> if (dslKind == DslKind.KOTLIN) "project(${quotedDep})" else "project(${quotedDep})"
       else -> if (dslKind == DslKind.KOTLIN) quotedDep else quotedDep
+    }.let {
+      if (coordinates.featureVariantName.isEmpty()) {
+        when (dslKind) {
+          DslKind.KOTLIN -> "($it)"
+          DslKind.GROOVY -> " $it"
+        }
+      } else if (coordinates.featureVariantName == "test-fixtures") {
+        when (dslKind) {
+          DslKind.KOTLIN -> "(testFixtures($it))"
+          DslKind.GROOVY -> " testFixtures($it)"
+        }
+      } else {
+        val capability = when (coordinates) {
+          // TODO could probably pass the group around in ProjectCoordinates
+          is ProjectCoordinates -> "[group]${coordinates.identifier}-${coordinates.featureVariantName}"
+          is ModuleCoordinates -> "${coordinates.identifier}-${coordinates.featureVariantName}"
+          else -> "[group]:${coordinates.identifier}-${coordinates.featureVariantName}"
+        }
+        val quote = when (dslKind) {
+          DslKind.KOTLIN -> "\""
+          DslKind.GROOVY -> "'"
+        }
+        "($it) { capabilities { requireCapabilities($quote$capability$quote) } }"
+      }
     }
   }
 
