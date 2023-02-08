a15c9a54285bb61ebf4687faf86c8405af896610
Simon Marquis <contact@simon-marquis.fr>
2023-02-08 22:53:38 +0100

    Add test to ensure text attribute reference is parsed as such

--- src/test/kotlin/com/autonomousapps/model/AttrRefTest.kt
+++ src/test/kotlin/com/autonomousapps/model/AttrRefTest.kt
@@ -1,6 +1,8 @@
 package com.autonomousapps.model
 
+import com.autonomousapps.model.AndroidResSource.AttrRef
 import com.autonomousapps.model.AndroidResSource.AttrRef.Companion.from
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 
@@ -14,6 +16,14 @@ class AttrRefTest {
   @Test
   fun `escaped question mark character followed by text is not parsed as an AttrRef`() {
     assertNull(from("android:text" to "\\?foo"))
+  }
+
+  @Test
+  fun `text attribute reference is parsed as an AttrRef`() {
+    assertEquals(
+      AttrRef(type = "attr", id = "foo"),
+      from("android:text" to "?foo"),
+    )
   }
 
 }
