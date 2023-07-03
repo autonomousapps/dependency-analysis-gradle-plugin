95a5aa306fed48d49473e577224cdb4d37b78ff7
Zac Sweers <pandanomic@gmail.com>
2023-07-03 13:24:18 -0400

    Add utilities for identifying KMP modules from attributes

--- src/main/kotlin/com/autonomousapps/model/GradleVariantIdentification.kt
+++ src/main/kotlin/com/autonomousapps/model/GradleVariantIdentification.kt
@@ -1,6 +1,8 @@
 package com.autonomousapps.model
 
+import com.autonomousapps.model.Coordinates.Companion.copy
 import com.squareup.moshi.JsonClass
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
 import java.io.Serializable
 
 @JsonClass(generateAdapter = false)
@@ -39,4 +41,30 @@ data class GradleVariantIdentification(
       target.gradleVariantIdentification.capabilities.containsAll(capabilities)
     }
   }
+}
+
+/**
+ * KMP artifacts have a [KotlinPlatformType] attribute defined for which target it is.
+ *
+ * At a general level, these can be thought of as [KotlinPlatformType.common] and "the rest", which
+ * covers specific platform targets like [KotlinPlatformType.jvm], [KotlinPlatformType.native], etc.
+ *
+ * When inspecting attributes for the context of DAGP, we want to know if a given dependency is a common
+ * dependency or a specific target. When recommending advice, we want to defer to the common targets as
+ * a sort of implicit bundle of its other targets.
+ */
+internal val GradleVariantIdentification.kmpAttribute get() = attributes[KotlinPlatformType.attribute.name]
+
+/** Returns whether this is a [KotlinPlatformType.common] dependency. */
+internal val GradleVariantIdentification.isKmpCommonTarget get() = kmpAttribute == KotlinPlatformType.common.name
+
+/** Returns whether this is a specific non-common [KotlinPlatformType] dependency, such as [KotlinPlatformType.jvm]. */
+internal val GradleVariantIdentification.isKmpNonCommonTarget get() = kmpAttribute != null && !isKmpCommonTarget
+
+/**
+ * Returns a [Coordinates.gav] string that represents the parent [Coordinates.identifier] of the given KMP target's
+ * `common` parent.
+ */
+internal fun ModuleCoordinates.kmpCommonParentIdentifier(): String {
+  return identifier.substringBeforeLast('-')
 }
