autonomousapps/dependency-analysis-gradle-plugin

commit d1815395a2bfc60eafc59b8f931b3f7c03fbda32

Consistently use IncludedBuildCoordinates to refer to other projects (#916)
* Consistently use IncludedBuildCoordinates to refer to other projects

There is a bug that leads to wrong/inconsistent advices if included
builds are involved. The assumption at the moment is that if a
'project path' matches the 'identity path', the dependency is not in
an included build.

This is in:
`gradleStrings.ResolvedArtifactResult.toCoordinates()`

While this works in the case where we do not use included builds (much),
it is generally wrong. There can also be references to other projects
inside other included builds. And, more problematic, an included
build can refer back to the "root" build. The plugin then assumes
that a a project in the root build could be addressed via
'project(":path")' notation from that other build, which is not true.

This is an attempt to use IncludedBuildCoordinates consistently
throughout the analysis. Then we also do not need to handle both
in the middle of the advice calculation. Then only in the end when
an advice is given, we decide which representation of the coordinates
is shown to the user. For 'add' advices, we need to track the build ID
(build name), to decide if a project is in the same build and can
hence be addressed by project coordinates.

* Track buildName in more places / add test coverage to (see #896)

* Tests: improve formatting and comments

* Add comment

* Remove redundant parentheses

* Move comments

* 'buildName' -> 'buildPath'

* Get buildPath of current build from the root component

src/main/kotlin/com/autonomousapps/tasks/ComputeAdviceTask.kt
@@ -40,6 +40,9 @@ abstract class ComputeAdviceTask @Inject constructor(
   @get:Input
   abstract val projectPath: Property<String>
 
+  @get:Input
+  abstract val buildPath: Property<String>
+
   @get:PathSensitive(PathSensitivity.RELATIVE)
   @get:InputFiles
   abstract val dependencyUsageReports: ListProperty<RegularFile>
@@ -88,6 +91,7 @@ abstract class ComputeAdviceTask @Inject constructor(
   @TaskAction fun action() {
     workerExecutor.noIsolation().submit(ComputeAdviceAction::class.java) {
       projectPath.set(this@ComputeAdviceTask.projectPath)
+      buildPath.set(this@ComputeAdviceTask.buildPath)
       dependencyUsageReports.set(this@ComputeAdviceTask.dependencyUsageReports)
       dependencyGraphViews.set(this@ComputeAdviceTask.dependencyGraphViews)
       androidScoreReports.set(this@ComputeAdviceTask.androidScoreReports)
@@ -106,6 +110,7 @@ abstract class ComputeAdviceTask @Inject constructor(
 
   interface ComputeAdviceParameters : WorkParameters {
     val projectPath: Property<String>
+    val buildPath: Property<String>
     val dependencyUsageReports: ListProperty<RegularFile>
     val dependencyGraphViews: ListProperty<RegularFile>
     val androidScoreReports: ListProperty<RegularFile>
@@ -130,7 +135,7 @@ abstract class ComputeAdviceTask @Inject constructor(
       val bundleTraces = parameters.bundledTraces.getAndDelete()
 
       val projectPath = parameters.projectPath.get()
-      val projectNode = ProjectCoordinates(projectPath, GradleVariantIdentification(emptySet(), emptyMap()))
+      val buildPath = parameters.buildPath.get()
       val declarations = parameters.declarations.fromJsonSet<Declaration>()
       val dependencyGraph = parameters.dependencyGraphViews.get()
         .map { it.fromJson<DependencyGraphView>() }
@@ -152,7 +157,7 @@ abstract class ComputeAdviceTask @Inject constructor(
       val isKaptApplied = parameters.kapt.get()
 
       val bundles = Bundles.of(
-        projectNode = projectNode,
+        projectPath = projectPath,
         dependencyGraph = dependencyGraph,
         bundleRules = bundleRules,
         dependencyUsages = dependencyUsages,
@@ -161,6 +166,7 @@ abstract class ComputeAdviceTask @Inject constructor(
 
       val dependencyAdviceBuilder = DependencyAdviceBuilder(
         projectPath = projectPath,
+        buildPath = buildPath,
         bundles = bundles,
         dependencyUsages = dependencyUsages,
         annotationProcessorUsages = annotationProcessorUsages,
@@ -225,6 +231,7 @@ internal class PluginAdviceBuilder(
 
 internal class DependencyAdviceBuilder(
   projectPath: String,
+  private val buildPath: String,
   private val bundles: Bundles,
   private val dependencyUsages: Map<Coordinates, Set<Usage>>,
   private val annotationProcessorUsages: Map<Coordinates, Set<Usage>>,
@@ -249,7 +256,7 @@ internal class DependencyAdviceBuilder(
     val declarations = declarations.filterToSet { Configurations.isForRegularDependency(it.configurationName) }
     return dependencyUsages.asSequence()
       .flatMap { (coordinates, usages) ->
-        StandardTransform(coordinates, declarations, supportedSourceSets).reduce(usages).map { it to coordinates }
+        StandardTransform(coordinates, declarations, supportedSourceSets, buildPath).reduce(usages).map { it to coordinates }
       }
       // "null" removes the advice
       .mapNotNull { (advice, originalCoordinates) ->
@@ -259,7 +266,11 @@ internal class DependencyAdviceBuilder(
         when {
           // The user cannot change this one:
           // https://github.com/gradle/gradle/blob/d9303339298e6206182fd1f5c7e51f11e4bdff30/subprojects/plugins/src/main/java/org/gradle/api/plugins/JavaTestFixturesPlugin.java#L68
-          advice.coordinates.identifier == projectPath && advice.fromConfiguration?.endsWith("testFixturesApi") ?: false -> {
+          advice.coordinates.identifier == projectPath && advice.fromConfiguration?.equals("testFixturesApi") ?: false -> {
+            null
+          }
+          // https://github.com/gradle/gradle/blob/d9303339298e6206182fd1f5c7e51f11e4bdff30/subprojects/plugins/src/main/java/org/gradle/api/plugins/JavaTestFixturesPlugin.java#L70
+          advice.coordinates.identifier == projectPath && advice.fromConfiguration?.lowercase()?.endsWith("testimplementation") ?: false -> {
             null
           }
           advice.isAdd() && bundles.hasParentInBundle(originalCoordinates) -> {
@@ -296,7 +307,7 @@ internal class DependencyAdviceBuilder(
     val declarations = declarations.filterToSet { Configurations.isForAnnotationProcessor(it.configurationName) }
     return annotationProcessorUsages.asSequence()
       .flatMap { (coordinates, usages) ->
-        StandardTransform(coordinates, declarations, supportedSourceSets, isKaptApplied).reduce(usages)
+        StandardTransform(coordinates, declarations, supportedSourceSets, buildPath, isKaptApplied).reduce(usages)
       }
   }
 }

src/main/kotlin/com/autonomousapps/tasks/ReasonTask.kt
@@ -180,7 +180,7 @@ abstract class ReasonTask @Inject constructor(
 
     override fun execute() {
       val reason = DependencyAdviceExplainer(
-        project = ProjectCoordinates(projectPath, GradleVariantIdentification(emptySet(), emptyMap())),
+        project = ProjectCoordinates(projectPath, GradleVariantIdentification(setOf("ROOT"), emptyMap()), ":"),
         target = coord,
         usages = usages,
         advice = finalAdvice,

src/functionalTest/groovy/com/autonomousapps/AdviceHelper.groovy
@@ -49,8 +49,8 @@ final class AdviceHelper {
     return projectCoordinates(dep.identifier)
   }
 
-  static ProjectCoordinates projectCoordinates(String projectPath, String capability = null) {
-    return new ProjectCoordinates(projectPath, defaultGVI(capability))
+  static ProjectCoordinates projectCoordinates(String projectPath, String capability = null, String buildPath = ':') {
+    return new ProjectCoordinates(projectPath, defaultGVI(capability), buildPath)
   }
 
   static Coordinates includedBuildCoordinates(String identifier, ProjectCoordinates resolvedProject, String capability = null) {

src/functionalTest/groovy/com/autonomousapps/jvm/IncludedBuildSpec.groovy
@@ -1,6 +1,7 @@
 package com.autonomousapps.jvm
 
 import com.autonomousapps.jvm.projects.IncludedBuildProject
+import com.autonomousapps.jvm.projects.IncludedBuildWithAnnotationProcessorProject
 import com.autonomousapps.jvm.projects.IncludedBuildWithSubprojectsProject
 
 import static com.autonomousapps.utils.Runner.build
@@ -19,12 +20,35 @@ final class IncludedBuildSpec extends AbstractJvmSpec {
     gradleProject = project.gradleProject
 
     when: 'build does not fail'
-    build(gradleVersion, gradleProject.rootDir, 'buildHealth')
+    build(gradleVersion, gradleProject.rootDir, ':buildHealth')
+    build(gradleVersion, gradleProject.rootDir, ':second-build:buildHealth')
 
-    then: 'and there is no advice'
-    assertThat(project.actualBuildHealth()).containsExactlyElementsIn(project.expectedBuildHealth)
+    then: 'the build health of the first build is as expected'
+    assertThat(project.actualBuildHealth()).containsExactlyElementsIn(project.expectedBuildHealth('second-build'))
+
+    and: 'the build health of the second build is as expected'
+    assertThat(project.actualBuildHealthOfSecondBuild()).containsExactlyElementsIn(project.expectedBuildHealthOfIncludedBuild(':'))
 
     where: 'This new feature only works for Gradle 7.3+'
+    gradleVersion << INCLUDED_BUILD_SUPPORT_GRADLE_VERSIONS
+  }
+
+  def "result of analysis does not change if root build of included build tree changes for projects without subprojects"() {
+    given:
+    def project = new IncludedBuildProject()
+    gradleProject = project.gradleProject
+
+    when: 'build running from second build root does not fail'
+    build(gradleVersion, new File(gradleProject.rootDir, 'second-build'), ':the-project:buildHealth')
+    build(gradleVersion, new File(gradleProject.rootDir, 'second-build'), ':buildHealth')
+
+    then: 'the build health of the first build is the same as when running that build directly'
+    assertThat(project.actualBuildHealth()).containsExactlyElementsIn(project.expectedBuildHealth(':'))
+
+    then: 'the build health of the second build is the same as when running that build as included build'
+    assertThat(project.actualBuildHealthOfSecondBuild()).containsExactlyElementsIn(project.expectedBuildHealthOfIncludedBuild('the-project'))
+
+    where:
     gradleVersion << INCLUDED_BUILD_SUPPORT_GRADLE_VERSIONS
   }
 
@@ -52,7 +76,7 @@ final class IncludedBuildSpec extends AbstractJvmSpec {
     build(gradleVersion, gradleProject.rootDir, ':second-build:buildHealth')
 
     then: 'and there is no advice'
-    assertThat(project.actualIncludedBuildHealth()).containsExactlyElementsIn(project.expectedIncludedBuildHealth)
+    assertThat(project.actualIncludedBuildHealth()).containsExactlyElementsIn(project.expectedIncludedBuildHealth('second-build'))
 
     where:
     gradleVersion << INCLUDED_BUILD_SUPPORT_GRADLE_VERSIONS
@@ -63,11 +87,74 @@ final class IncludedBuildSpec extends AbstractJvmSpec {
     def project = new IncludedBuildWithSubprojectsProject(true)
     gradleProject = project.gradleProject
 
-    when: 'build does not fail'
+    when:
     build(gradleVersion, gradleProject.rootDir, ':second-build:buildHealth')
 
-    then: 'and there is no advice'
-    assertThat(project.actualIncludedBuildHealth()).containsExactlyElementsIn(project.expectedIncludedBuildHealth)
+    then:
+    assertThat(project.actualIncludedBuildHealth()).containsExactlyElementsIn(project.expectedIncludedBuildHealth('second-build'))
+
+    where:
+    gradleVersion << INCLUDED_BUILD_SUPPORT_GRADLE_VERSIONS
+  }
+
+  def "result of analysis does not change if root build of included build tree changes for projects with subprojects"() {
+    given:
+    def project = new IncludedBuildWithSubprojectsProject(true)
+    gradleProject = project.gradleProject
+
+    when:
+    build(gradleVersion, new File(gradleProject.rootDir, 'second-build'), ':buildHealth')
+
+    then:
+    assertThat(project.actualIncludedBuildHealth()).containsExactlyElementsIn(project.expectedIncludedBuildHealth(':'))
+
+    where:
+    gradleVersion << INCLUDED_BUILD_SUPPORT_GRADLE_VERSIONS
+  }
+
+  def "up-to-date check is correct when switching root builds"() {
+    given:
+    def project = new IncludedBuildWithSubprojectsProject(true)
+    gradleProject = project.gradleProject
+
+    when: 'The first build is the root'
+    build(gradleVersion, gradleProject.rootDir, ':second-build:buildHealth')
+
+    then:
+    assertThat(project.actualIncludedBuildHealth()).containsExactlyElementsIn(project.expectedIncludedBuildHealth('second-build'))
+
+    when: 'The second build is the root - the "buildPath" attribute of ProjectCoordinates changes'
+    build(gradleVersion, new File(gradleProject.rootDir, 'second-build'), ':buildHealth')
+
+    then:
+    assertThat(project.actualIncludedBuildHealth()).containsExactlyElementsIn(project.expectedIncludedBuildHealth(':'))
+
+    where:
+    gradleVersion << INCLUDED_BUILD_SUPPORT_GRADLE_VERSIONS
+  }
+
+  def "can handle annotation processors from cache in subsequent builds"() {
+    given:
+    def project = new IncludedBuildWithAnnotationProcessorProject()
+    gradleProject = project.gradleProject
+
+    when:
+    build(gradleVersion, gradleProject.rootDir, '--build-cache', ':buildHealth')
+
+    then:
+    assertThat(project.actualBuildHealth()).containsExactlyElementsIn(project.expectedBuildHealth)
+
+    when: 'Running again - UP-TO-DATE'
+    build(gradleVersion, gradleProject.rootDir, '--build-cache', ':buildHealth')
+
+    then: 'Result is the same'
+    assertThat(project.actualBuildHealth()).containsExactlyElementsIn(project.expectedBuildHealth)
+
+    when: 'Running again - FROM-CACHE'
+    build(gradleVersion, gradleProject.rootDir, '--build-cache', 'clean', ':buildHealth')
+
+    then: 'Result is the same'
+    assertThat(project.actualBuildHealth()).containsExactlyElementsIn(project.expectedBuildHealth)
 
     where:
     gradleVersion << INCLUDED_BUILD_SUPPORT_GRADLE_VERSIONS

src/functionalTest/groovy/com/autonomousapps/jvm/projects/IncludedBuildProject.groovy
@@ -21,6 +21,10 @@ final class IncludedBuildProject extends AbstractProject {
       root.withBuildScript { bs ->
         bs.plugins.add(Plugin.javaLibraryPlugin)
         bs.dependencies = [new Dependency('implementation', 'second:second-build:1.0')]
+        bs.additions = """\
+          group = 'first'
+          version = '1.0'
+        """.stripIndent()
       }
       root.settingsScript.additions = """\
         includeBuild 'second-build'
@@ -38,12 +42,16 @@ final class IncludedBuildProject extends AbstractProject {
     }
     builder.withIncludedBuild('second-build') { second ->
       second.withBuildScript { bs ->
-        bs.plugins = [Plugin.javaLibraryPlugin]
+        bs.plugins.add(Plugin.javaLibraryPlugin)
+        bs.dependencies = [new Dependency('testImplementation', 'first:the-project:1.0')]
         bs.additions = """\
           group = 'second'
           version = '1.0'
         """.stripIndent()
       }
+      second.settingsScript.additions = """\
+        includeBuild('..') { name = 'the-project' }
+      """.stripIndent()
       second.sources = [
         new Source(
           SourceType.JAVA, 'Second', 'com/example/included',
@@ -65,12 +73,27 @@ final class IncludedBuildProject extends AbstractProject {
     return actualProjectAdvice(gradleProject)
   }
 
-  final Set<ProjectAdvice> expectedBuildHealth = [
+  Set<ProjectAdvice> actualBuildHealthOfSecondBuild() {
+    def included = gradleProject.includedBuilds[0]
+    def project = new GradleProject(new java.io.File(gradleProject.rootDir, 'second-build'), null, included, [], [])
+    return actualProjectAdvice(project)
+  }
+
+  static Set<ProjectAdvice> expectedBuildHealth(String buildPathInAdvice) {[
     projectAdviceForDependencies(':', [
       Advice.ofRemove(
-        includedBuildCoordinates('second:second-build', projectCoordinates(':', 'second:second-build')),
+        includedBuildCoordinates('second:second-build', projectCoordinates(':', 'second:second-build', buildPathInAdvice)),
         'implementation'
       )
     ] as Set<Advice>)
-  ]
+  ]}
+
+  static Set<ProjectAdvice> expectedBuildHealthOfIncludedBuild(String buildPathInAdvice) {[
+    projectAdviceForDependencies(':', [
+      Advice.ofRemove(
+        includedBuildCoordinates('first:the-project', projectCoordinates(':', 'first:the-project', buildPathInAdvice)),
+        'testImplementation'
+      )
+    ] as Set<Advice>)
+  ]}
 }

src/functionalTest/groovy/com/autonomousapps/jvm/projects/IncludedBuildWithAnnotationProcessorProject.groovy
@@ -1,0 +1,126 @@
+package com.autonomousapps.jvm.projects
+
+import com.autonomousapps.AbstractProject
+import com.autonomousapps.kit.*
+import com.autonomousapps.model.Advice
+import com.autonomousapps.model.ProjectAdvice
+
+import static com.autonomousapps.AdviceHelper.*
+
+final class IncludedBuildWithAnnotationProcessorProject extends AbstractProject {
+
+  final GradleProject gradleProject
+
+  IncludedBuildWithAnnotationProcessorProject() {
+    this.gradleProject = build()
+  }
+
+  private GradleProject build() {
+    def builder = newGradleProjectBuilder()
+    builder.withRootProject { root ->
+      root.settingsScript.additions = """\
+        includeBuild 'processor-build'
+        include 'user-of-processor'
+      """.stripIndent()
+    }
+    builder.withSubprojectInIncludedBuild('processor-build', 'sub-processor1') { secondSub ->
+      secondSub.withBuildScript { bs ->
+        bs.plugins = [Plugin.javaLibraryPlugin]
+        bs.dependencies = [
+          new Dependency('annotationProcessor', 'com.google.auto.service:auto-service:1.0-rc6'),
+          new Dependency('compileOnly', 'com.google.auto.service:auto-service-annotations:1.0-rc6'),
+        ]
+        bs.repositories = [
+          Repository.GOOGLE,
+          Repository.MAVEN_CENTRAL
+        ]
+        bs.additions = """\
+          group = 'my.custom.processor'
+        """.stripIndent()
+      }
+      secondSub.sources = [
+        new Source(
+          SourceType.JAVA, 'Field', 'com/example/included/processor1',
+          """\
+        package com.example.included.processor1;
+        
+        import java.lang.annotation.ElementType;
+        import java.lang.annotation.Retention;
+        import java.lang.annotation.RetentionPolicy;
+        import java.lang.annotation.Target;
+
+        @Retention(RetentionPolicy.RUNTIME)
+        @Target({ElementType.FIELD})
+        public @interface Field {
+        }
+          """.stripIndent()
+        ),
+        new Source(
+          SourceType.JAVA, 'Processor1', 'com/example/included/processor1',
+          """\
+        package com.example.included.processor1;
+        
+        import java.util.HashSet;
+        import java.util.List;
+        import java.util.Set;
+        import javax.annotation.processing.AbstractProcessor;
+        import javax.annotation.processing.Processor;
+        import javax.annotation.processing.RoundEnvironment;
+        import javax.lang.model.element.TypeElement;
+        
+        import com.google.auto.service.AutoService;
+
+        @AutoService(Processor.class)
+        public class Processor1 extends AbstractProcessor {
+          @Override
+          public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment env) {
+            return true;
+          }
+
+          @Override
+          public Set<String> getSupportedAnnotationTypes() {
+            return new HashSet<>(List.of(Field.class.getName()));
+          }
+        }
+          """.stripIndent()
+        )
+      ]
+    }
+    builder.withSubproject("user-of-processor", userOfProcessor -> {
+      userOfProcessor.withBuildScript {bs ->
+        bs.plugins = [Plugin.javaLibraryPlugin]
+        bs.dependencies = [
+          new Dependency('api', 'my.custom.processor:sub-processor1'),
+          new Dependency('annotationProcessor', 'my.custom.processor:sub-processor1'),
+        ]
+      }
+
+      userOfProcessor.sources = [
+        new Source(
+          SourceType.JAVA, 'UserOfProcessor', 'com/example/user/of/processor1',
+          """\
+        package com.example.user.of.processor1;
+        
+        import com.example.included.processor1.Field;
+
+        public class UserOfProcessor {
+          public @Field int field;
+        }
+          """.stripIndent()
+        )
+      ]
+    })
+
+    def project = builder.build()
+    project.writer().write()
+    return project
+  }
+
+  Set<ProjectAdvice> actualBuildHealth() {
+    return actualProjectAdvice(gradleProject)
+  }
+
+  final Set<ProjectAdvice> expectedBuildHealth = [
+    projectAdviceForDependencies(':user-of-processor', [] as Set<Advice>)
+  ]
+}

src/functionalTest/groovy/com/autonomousapps/jvm/projects/IncludedBuildWithSubprojectsProject.groovy
@@ -10,12 +10,14 @@ import static com.autonomousapps.AdviceHelper.*
 final class IncludedBuildWithSubprojectsProject extends AbstractProject {
 
   final GradleProject gradleProject
+  final boolean useProjectDependencyWherePossible
 
   IncludedBuildWithSubprojectsProject(boolean useProjectDependencyWherePossible = false) {
-    this.gradleProject = build(useProjectDependencyWherePossible)
+    this.useProjectDependencyWherePossible = useProjectDependencyWherePossible
+    this.gradleProject = build()
   }
 
-  private GradleProject build(boolean useProjectDependencyWherePossible) {
+  private GradleProject build() {
     def builder = newGradleProjectBuilder()
     builder.withRootProject { root ->
       root.withBuildScript { bs ->
@@ -42,11 +44,11 @@ final class IncludedBuildWithSubprojectsProject extends AbstractProject {
     }
     builder.withSubprojectInIncludedBuild('second-build', 'second-sub1') { secondSub ->
       secondSub.withBuildScript { bs ->
-        bs.plugins = [Plugin.javaLibraryPlugin]
+        bs.plugins.add(Plugin.javaLibraryPlugin)
         if (useProjectDependencyWherePossible) {
-          bs.dependencies = [new Dependency('implementation', ':second-sub2')]
+          bs.dependencies = [new Dependency('api', ':second-sub2')]
         } else {
-          bs.dependencies = [new Dependency('implementation', 'second:second-sub2')]
+          bs.dependencies = [new Dependency('api', 'second:second-sub2')]
         }
         bs.additions = """\
           group = 'second'
@@ -107,8 +109,12 @@ final class IncludedBuildWithSubprojectsProject extends AbstractProject {
     return actualProjectAdvice(project)
   }
 
-  final Set<ProjectAdvice> expectedIncludedBuildHealth = [
-    projectAdviceForDependencies(':second-sub1', [] as Set<Advice>),
+  final Set<ProjectAdvice> expectedIncludedBuildHealth(String buildPathInAdvice) {[
+    projectAdviceForDependencies(':second-sub1', [
+      useProjectDependencyWherePossible
+        ? Advice.ofChange(projectCoordinates(':second-sub2', null, buildPathInAdvice), 'api', 'implementation')
+        : Advice.ofChange(includedBuildCoordinates('second:second-sub2', projectCoordinates(':second-sub2', 'second:second-sub2', buildPathInAdvice)), 'api', 'implementation')
+    ] as Set<Advice>),
     projectAdviceForDependencies(':second-sub2', [] as Set<Advice>)
-  ]
+  ]}
 }

commit dd1c3d3f8e172221b729be2f612807778913a1da

Fix addressing a project by path in the reason '--id' argument
This broke with #916

src/main/kotlin/com/autonomousapps/internal/utils/coordinatesStrings.kt
@@ -1,0 +1,34 @@
+package com.autonomousapps.internal.utils
+
+import com.autonomousapps.model.Coordinates
+import com.autonomousapps.model.IncludedBuildCoordinates
+import com.autonomousapps.model.intermediates.Usage
+
+internal fun Map<Coordinates, Set<Usage>>.toStringCoordinates(buildPath: String): Map<String, Set<Usage>> =
+  map { (key, value) ->
+    toCoordinatesKey(key, buildPath) to value
+  }.toMap()
+
+internal fun toCoordinatesKey(coordinates: Coordinates, buildPath: String) =
+  coordinates.gav() + when {
+    // If the coordinates point at a project in the current build, there are two notations to address it:
+    // 1) 'group:name' coordinates, which is the main representation (see IncludedBuildCoordinates)
+    // 2) The project path notation starting with a ':' (:project-name)
+    // To allow notation 2) to address a project in the 'reasons' task, we add this coordinate to the key as well if applicable (separated by '|')
+    coordinates is IncludedBuildCoordinates && coordinates.resolvedProject.buildPath == buildPath -> "|${coordinates.resolvedProject.gav()}"
+    else -> ""
+  }
+
+internal fun <T> String.equalsKey(mapEntry: Map.Entry<String, T>) =
+  mapEntry.firstKeySegment() == this || mapEntry.secondKeySegment() == this
+
+internal fun <T> String.startsWithKey(mapEntry: Map.Entry<String, T>) =
+  mapEntry.firstKeySegment().startsWith(this) || mapEntry.secondKeySegment()?.startsWith(this) == true
+
+/** First key segment is always 'group:name' coordinates */
+internal fun <T> Map.Entry<String, T>.firstKeySegment(): String =
+  if (key.contains("|")) key.split("|")[0] else key
+
+/** Key might contain a second segment for alternative project path notation ':project-name' */
+internal fun <T> Map.Entry<String, T>.secondKeySegment(): String? =
+  if (key.contains("|")) key.split("|")[1] else null

src/main/kotlin/com/autonomousapps/tasks/ComputeAdviceTask.kt
@@ -190,15 +190,11 @@ abstract class ComputeAdviceTask @Inject constructor(
 
       output.bufferWriteJson(projectAdvice)
       // These must be transformed so that the Coordinates are Strings for serialization
-      dependencyUsagesOut.bufferWriteJsonMap(dependencyUsages.toStringCoordinates())
-      annotationProcessorUsagesOut.bufferWriteJsonMap(annotationProcessorUsages.toStringCoordinates())
+      dependencyUsagesOut.bufferWriteJsonMap(dependencyUsages.toStringCoordinates(buildPath))
+      annotationProcessorUsagesOut.bufferWriteJsonMap(annotationProcessorUsages.toStringCoordinates(buildPath))
       // TODO consider centralizing this logic in a separate PR
       bundleTraces.bufferWriteJsonSet(dependencyAdviceBuilder.bundledTraces)
     }
-
-    private fun Map<Coordinates, Set<Usage>>.toStringCoordinates(): Map<String, Set<Usage>> = map { (key, value) ->
-      key.gav() to value
-    }.toMap()
   }
 }
 

src/main/kotlin/com/autonomousapps/tasks/ReasonTask.kt
@@ -205,10 +205,10 @@ abstract class ReasonTask @Inject constructor(
         }
 
       // Guaranteed to find full GAV or throw
-      val gav = dependencyUsages.entries.find(requestedId::equalsKey)?.key
-        ?: dependencyUsages.entries.find(requestedId::startsWithKey)?.key
-        ?: annotationProcessorUsages.entries.find(requestedId::equalsKey)?.key
-        ?: annotationProcessorUsages.entries.find(requestedId::startsWithKey)?.key
+      val gav = dependencyUsages.entries.find(requestedId::equalsKey)?.firstKeySegment()
+        ?: dependencyUsages.entries.find(requestedId::startsWithKey)?.firstKeySegment()
+        ?: annotationProcessorUsages.entries.find(requestedId::equalsKey)?.firstKeySegment()
+        ?: annotationProcessorUsages.entries.find(requestedId::startsWithKey)?.firstKeySegment()
         ?: findInGraph()
         ?: throw InvalidUserDataException("There is no dependency with coordinates '$requestedId' in this project.")
       return Coordinates.of(gav)
@@ -283,6 +283,3 @@ abstract class ReasonTask @Inject constructor(
   }
 
 }
-
-private fun <T> String.equalsKey(mapEntry: Map.Entry<String, T>) = mapEntry.key == this
-private fun <T> String.startsWithKey(mapEntry: Map.Entry<String, T>) = mapEntry.key.startsWith(this)

src/functionalTest/groovy/com/autonomousapps/jvm/JvmReasonSpec.groovy
@@ -1,0 +1,52 @@
+package com.autonomousapps.jvm
+
+import com.autonomousapps.AbstractFunctionalSpec
+import com.autonomousapps.jvm.projects.NestedSubprojectsProject
+import com.autonomousapps.utils.Colors
+import org.gradle.testkit.runner.BuildResult
+
+import static com.autonomousapps.utils.Runner.build
+import static com.google.common.truth.Truth.assertThat
+
+final class JvmReasonSpec extends AbstractFunctionalSpec {
+
+  def "can discover reason for project dependency defined by project path (#gradleVersion)"() {
+    given:
+    gradleProject = new NestedSubprojectsProject().gradleProject
+    def id = ':featureA:public'
+
+    when:
+    def result = build(gradleVersion, gradleProject.rootDir, ':featureC:public:reason', '--id', id)
+
+    then:
+    outputMatchesForProject(result)
+
+    where:
+    gradleVersion << gradleVersions()
+  }
+
+  def "can discover reason for project dependency defined by coordinates (#gradleVersion)"() {
+    given:
+    gradleProject = new NestedSubprojectsProject().gradleProject
+    def id = 'the-project.featureA:public'
+
+    when:
+    def result = build(gradleVersion, gradleProject.rootDir, ':featureC:public:reason', '--id', id)
+
+    then:
+    outputMatchesForProject(result)
+
+    where:
+    gradleVersion << gradleVersions()
+  }
+
+  private static void outputMatchesForProject(BuildResult result) {
+    def lines = Colors.decolorize(result.output).readLines()
+    def asked = lines.find { it.startsWith('You asked about') }
+    def advised = lines.find { it.startsWith('There is no advice') }
+
+    assertThat(asked).isEqualTo("You asked about the dependency 'the-project.featureA:public'.")
+    assertThat(advised).isEqualTo("There is no advice regarding this dependency.")
+  }
+
+}