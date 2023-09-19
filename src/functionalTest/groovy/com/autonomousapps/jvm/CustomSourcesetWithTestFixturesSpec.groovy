72b09626d793fee3d57962dc7d7545f2f71d2215
timvand <tim.vanderlippe@adyen.com>
2023-09-19 11:56:04 -0700

    Add test to showcase issue with custom sourceset and testFixtures

---
+++ src/functionalTest/groovy/com/autonomousapps/jvm/CustomSourcesetWithTestFixturesSpec.groovy
@@ -0,0 +1,25 @@
+package com.autonomousapps.jvm
+
+import com.autonomousapps.jvm.projects.CustomSourcesetWithTestFixturesProject
+
+import static com.autonomousapps.utils.Runner.build
+import static com.google.common.truth.Truth.assertThat
+
+final class CustomSourcesetWithTestFixturesSpec extends AbstractJvmSpec {
+
+  def "unused test dependencies are reported (#gradleVersion)"() {
+    given:
+    def project = new CustomSourcesetWithTestFixturesProject()
+    gradleProject = project.gradleProject
+
+    when:
+    build(gradleVersion, gradleProject.rootDir, 'buildHealth')
+
+    then:
+    assertThat(project.actualBuildHealth()).containsExactlyElementsIn(project.expectedBuildHealth)
+
+    where:
+    gradleVersion << gradleVersions()
+  }
+
+}
---
+++ src/functionalTest/groovy/com/autonomousapps/jvm/projects/CustomSourcesetWithTestFixturesProject.groovy
@@ -0,0 +1,115 @@
+package com.autonomousapps.jvm.projects
+
+import com.autonomousapps.AbstractProject
+import com.autonomousapps.kit.GradleProject
+import com.autonomousapps.kit.Plugin
+import com.autonomousapps.kit.Source
+import com.autonomousapps.kit.SourceType
+import com.autonomousapps.model.Advice
+import com.autonomousapps.model.ProjectAdvice
+
+import static com.autonomousapps.AdviceHelper.*
+import static com.autonomousapps.kit.Dependency.*
+
+final class CustomSourcesetWithTestFixturesProject extends AbstractProject {
+
+  final GradleProject gradleProject
+
+  CustomSourcesetWithTestFixturesProject() {
+    this.gradleProject = build()
+  }
+
+  private GradleProject build() {
+    def builder = newGradleProjectBuilder()
+    builder.withSubproject('producer') { s ->
+      s.sources = sources
+      s.withBuildScript { bs ->
+        bs.plugins = [Plugin.javaLibraryPlugin, Plugin.javaTestFixturesPlugin]
+        bs.dependencies = [
+          commonsCollections('api'),
+          commonsCollections('testFixturesApi')
+        ]
+        bs.additions = """
+        sourceSets {
+            customTestSourceset {
+                java {
+                   compileClasspath += main.output
+                   runtimeClasspath += main.output
+                }
+            }
+        }
+        """
+      }
+    }
+    builder.withSubproject('consumer') { s ->
+      s.sources = consumerTestSources
+      s.withBuildScript { bs ->
+        bs.plugins = [Plugin.javaLibraryPlugin]
+        bs.dependencies = [
+          project('testImplementation', ':producer', 'test-fixtures')
+        ]
+      }
+    }
+
+    def project = builder.build()
+    project.writer().write()
+    return project
+  }
+
+  private sources = [
+    new Source(
+      SourceType.JAVA, "Example", "com/example",
+      """\
+        package com.example;
+        
+        import org.apache.commons.collections4.bag.HashBag;
+        
+        public class Example {
+          public HashBag<String> bag;
+        }""".stripIndent()
+    ),
+    new Source(
+      SourceType.JAVA, "ExampleFixture", "com/example/fixtures",
+      """\
+        package com.example.fixtures;
+        
+        import org.apache.commons.collections4.bag.HashBag;
+        
+        public class ExampleFixture {
+          private HashBag<String> internalBag;
+        }""".stripIndent(),
+      "testFixtures"
+    )
+  ]
+
+  private consumerTestSources = [
+    new Source(
+      SourceType.JAVA, "ConsumerTest", "com/example/consumer/test",
+      """\
+        package com.example.consumer.test;
+        
+        import com.example.fixtures.ExampleFixture;
+        
+        public class ConsumerTest {
+          private ExampleFixture fixture;
+        }""".stripIndent(),
+      "test"
+    )
+  ]
+
+  Set<ProjectAdvice> actualBuildHealth() {
+    return actualProjectAdvice(gradleProject)
+  }
+
+  private final Set<Advice> expectedProducerAdvice = [
+    Advice.ofChange(moduleCoordinates(commonsCollections('')), 'testFixturesApi', 'testFixturesImplementation'),
+  ]
+
+  final Set<ProjectAdvice> expectedBuildHealth() {[
+    emptyProjectAdviceFor(':consumer'),
+    ignoreSourceTestFixturesSet
+      ? emptyProjectAdviceFor(':producer')
+      : projectAdviceForDependencies(':producer', expectedProducerAdvice)
+  ]}
+
+}
