83bd478e1e6a261347def5540d032cf04003c5bd
Tony Robalik <tony.robalik@gmail.com>
2023-09-28 22:00:08 -0700

    Resolve Kotlin deprecations.

--- src/main/kotlin/com/autonomousapps/internal/utils/utils.kt
+++ src/main/kotlin/com/autonomousapps/internal/utils/utils.kt
@@ -132,11 +132,11 @@ internal fun String.capitalizeSafely(locale: Locale = Locale.ROOT): String {
     val firstChar = this[0]
     if (firstChar.isLowerCase()) {
       return buildString {
-        val titleChar = firstChar.toTitleCase()
-        if (titleChar != firstChar.toUpperCase()) {
+        val titleChar = firstChar.titlecaseChar()
+        if (titleChar != firstChar.uppercaseChar()) {
           append(titleChar)
         } else {
-          append(this@capitalizeSafely.substring(0, 1).toUpperCase(locale))
+          append(this@capitalizeSafely.substring(0, 1).uppercase(locale))
         }
         append(this@capitalizeSafely.substring(1))
       }
