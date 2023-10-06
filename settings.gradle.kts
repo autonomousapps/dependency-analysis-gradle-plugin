e202d7d4fec2bf6f90f078b721762dbe963da0e1
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-10-06 15:06:44 +0200

    Update 'com.gradle.enterprise' to 3.15.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,7 +26,7 @@ pluginManagement {
   plugins {
     id("com.autonomousapps.dependency-analysis") version "1.23.1"//latestSnapshot
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("com.gradle.enterprise") version "3.10.2"
+    id("com.gradle.enterprise") version "3.15.1"
     id("com.gradle.plugin-publish") version "1.1.0"
     id("org.jetbrains.kotlin.jvm") version "1.9.10"
     id("org.jetbrains.dokka") version "1.6.10"
