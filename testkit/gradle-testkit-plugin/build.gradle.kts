28de6e5ee700a2eba2cee21786e751cf70721f45
Tony Robalik <tony.robalik@gmail.com>
2023-11-18 22:46:02 -0800

    Prepare next development version of testkit-plugin.

--- testkit/gradle-testkit-plugin/build.gradle.kts
+++ testkit/gradle-testkit-plugin/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
   id("com.autonomousapps.testkit")
 }
 
-version = "0.6"
+version = "0.7-SNAPSHOT"
 val isSnapshot: Boolean = version.toString().endsWith("SNAPSHOT")
 val isRelease: Boolean = !isSnapshot
 
