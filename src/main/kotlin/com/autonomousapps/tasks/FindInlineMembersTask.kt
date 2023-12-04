12b80a3568dd6872406ee6181082d679d64d11cb
Simon Marquis <contact@simon-marquis.fr>
2023-12-04 07:24:38 +0000

    Linkify error report in `FindInlineMembersTask.kt`

--- src/main/kotlin/com/autonomousapps/tasks/FindInlineMembersTask.kt
+++ src/main/kotlin/com/autonomousapps/tasks/FindInlineMembersTask.kt
@@ -102,7 +102,7 @@ abstract class FindInlineMembersTask @Inject constructor(
       if (!finder.didWriteErrors) {
         errorsReport.writeText("")
       } else {
-        logger.warn("There were errors during inline member analysis. See ${errorsReport.path}")
+        logger.warn("There were errors during inline member analysis. See ${errorsReport.toUri()}")
       }
     }
   }
