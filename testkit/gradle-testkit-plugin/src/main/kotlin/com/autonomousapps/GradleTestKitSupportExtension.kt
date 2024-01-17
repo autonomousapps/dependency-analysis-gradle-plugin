47ab5f1e04e6bff856d1c2b71ba227580949f7e3
Tony Robalik <tony.robalik@gmail.com>
2024-01-17 10:21:07 -0800

    fix(testkit-plugin): use correct latest support version (0.15 not 0.16) in plugin.

--- testkit/gradle-testkit-plugin/src/main/kotlin/com/autonomousapps/GradleTestKitSupportExtension.kt
+++ testkit/gradle-testkit-plugin/src/main/kotlin/com/autonomousapps/GradleTestKitSupportExtension.kt
@@ -248,7 +248,7 @@ public abstract class GradleTestKitSupportExtension(private val project: Project
 
   internal companion object {
 
-    private const val DEFAULT_SUPPORT_VERSION = "0.16"
+    private const val DEFAULT_SUPPORT_VERSION = "0.15"
     private const val DEFAULT_TRUTH_VERSION = "1.6"
 
     fun create(project: Project): GradleTestKitSupportExtension {
