98556cf3658cb0ad0f4cb2be889bfb09a417ac88
Gaëtan Muller <m.gaetan89@gmail.com>
2024-07-25 09:16:15 +0200

    Update to AGP 8.6.0-beta02

--- src/functionalTest/groovy/com/autonomousapps/android/AbstractAndroidSpec.groovy
+++ src/functionalTest/groovy/com/autonomousapps/android/AbstractAndroidSpec.groovy
@@ -20,7 +20,7 @@ abstract class AbstractAndroidSpec extends AbstractFunctionalSpec {
   protected static final AGP_8_3 = AgpVersion.version('8.3.2')
   protected static final AGP_8_4 = AgpVersion.version('8.4.2')
   protected static final AGP_8_5 = AgpVersion.version('8.5.2')
-  protected static final AGP_8_6 = AgpVersion.version('8.6.0-beta01')
+  protected static final AGP_8_6 = AgpVersion.version('8.6.0-beta02')
   protected static final AGP_8_7 = AgpVersion.version('8.7.0-alpha02')
 
   protected static final AGP_LATEST = AGP_8_7
