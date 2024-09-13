2473ff2cdfb82989bdd4cb5e97063ad426e07442
Tony Robalik <tony.robalik@gmail.com>
2024-09-13 16:01:30 -0700

    test: reproduce issue 1215.

--- src/functionalTest/groovy/com/autonomousapps/jvm/AbiSpec.groovy
+++ src/functionalTest/groovy/com/autonomousapps/jvm/AbiSpec.groovy
@@ -2,7 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.autonomousapps.jvm
 
+import com.autonomousapps.jvm.projects.AbiImplementationProject
 import com.autonomousapps.jvm.projects.AbiProject
+import spock.lang.PendingFeature
 
 import static com.autonomousapps.utils.Runner.build
 import static com.google.common.truth.Truth.assertThat
@@ -12,6 +14,22 @@ final class AbiSpec extends AbstractJvmSpec {
   def "properties on internal classes are not part of the ABI (#gradleVersion)"() {
     given:
     def project = new AbiProject()
+    gradleProject = project.gradleProject
+
+    when:
+    build(gradleVersion, gradleProject.rootDir, 'buildHealth')
+
+    then:
+    assertThat(project.actualProjectAdvice()).containsExactlyElementsIn(project.expectedProjectAdvice)
+
+    where:
+    gradleVersion << gradleVersions()
+  }
+
+  @PendingFeature(reason = "https://github.com/autonomousapps/dependency-analysis-gradle-plugin/issues/1215")
+  def "class annotations are implementation when not used as annotations (#gradleVersion)"() {
+    given:
+    def project = new AbiImplementationProject()
     gradleProject = project.gradleProject
 
     when:
