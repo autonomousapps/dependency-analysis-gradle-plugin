56f26e0f2b57ffbb54db7ea5e7f3cf49577146c6
Tony Robalik <tony.robalik@gmail.com>
2024-09-13 14:50:01 -0700

    chore: require res references always use '_' instead of '.'

--- src/main/kotlin/com/autonomousapps/internal/parse/explodeAndroidResSource.kt
+++ src/main/kotlin/com/autonomousapps/internal/parse/explodeAndroidResSource.kt
@@ -14,7 +14,6 @@ import com.autonomousapps.internal.utils.document.mapNotNull
 import com.autonomousapps.internal.utils.filterToOrderedSet
 import com.autonomousapps.internal.utils.mapToSet
 import com.autonomousapps.model.AndroidResSource
-import com.autonomousapps.model.AndroidResSource.AttrRef.Companion.toCanonicalResString
 import org.w3c.dom.Document
 import org.xml.sax.SAXParseException
 import java.io.File
@@ -82,10 +81,7 @@ internal class AndroidResParser(
     doc.getElementsByTagName("style").mapNotNull {
       it.attributes.getNamedItem("parent")?.nodeValue
     }.mapToSet {
-      // Transform Theme.AppCompat.Light.DarkActionBar to Theme_AppCompat_Light_DarkActionBar
-      it.toCanonicalResString()
-    }.mapToSet {
-      AndroidResSource.StyleParentRef(it)
+      AndroidResSource.StyleParentRef.of(it)
     }
 
   private fun extractAttrsFromResourceXml(doc: Document) {
@@ -151,7 +147,7 @@ internal class AndroidManifestParser(
     val file = first
     val parseResult = second
     val applicationName = parseResult.applicationName
-    val theme = AndroidResSource.AttrRef.style(parseResult.theme.toCanonicalResString())
+    val theme = AndroidResSource.AttrRef.style(parseResult.theme)
     return ExplodedManifest(
       relativePath = file.toRelativeString(projectDir),
       applicationName = applicationName,
