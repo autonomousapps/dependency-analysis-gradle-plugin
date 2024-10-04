607bad3f7b90cdb6e8c775a94c25cf39956ed43b
Tony Robalik <tony.robalik@gmail.com>
2024-10-03 22:29:37 -0700

    fix: @Metadata's d2 array should typically be compileOnly.

--- src/functionalTest/groovy/com/autonomousapps/jvm/AnnotationsImplementationSpec.groovy
+++ src/functionalTest/groovy/com/autonomousapps/jvm/AnnotationsImplementationSpec.groovy
@@ -1,5 +1,6 @@
 package com.autonomousapps.jvm
 
+import com.autonomousapps.jvm.projects.AnnotationsCompileOnlyProject
 import com.autonomousapps.jvm.projects.AnnotationsImplementationProject
 
 import static com.autonomousapps.utils.Runner.build
@@ -14,6 +15,23 @@ final class AnnotationsImplementationSpec extends AbstractJvmSpec {
 
     when:
     build(gradleVersion, gradleProject.rootDir, 'buildHealth')
+
+    then:
+    assertThat(project.actualBuildHealth()).containsExactlyElementsIn(project.expectedBuildHealth)
+
+    where:
+    gradleVersion << gradleVersions()
+  }
+
+  def "classes used in compile-retained annotations are compileOnly (#gradleVersion)"() {
+    given:
+    def project = new AnnotationsCompileOnlyProject()
+    gradleProject = project.gradleProject
+
+    when:
+    build(gradleVersion, gradleProject.rootDir, 'buildHealth')
+    // TODO(tsr): still need better tests for reason. Before the fix, this output was wrong. Still not fixed really.
+    //, ':consumer:reason', '--id', 'org.jetbrains:annotations')
 
     then:
     assertThat(project.actualBuildHealth()).containsExactlyElementsIn(project.expectedBuildHealth)
