b3fbda7b633e4cbe25e7addda0f0d8424b946b0d
Tony Robalik <tony.robalik@gmail.com>
2024-11-01 13:24:08 -0700

    fix: differentiate visible from invisible annotations.

---
+++ src/functionalTest/groovy/com/autonomousapps/jvm/projects/AnnotationByDelegateProject.groovy
@@ -0,0 +1,58 @@
+package com.autonomousapps.jvm.projects
+
+import com.autonomousapps.AbstractProject
+import com.autonomousapps.kit.GradleProject
+import com.autonomousapps.kit.Source
+import com.autonomousapps.model.ProjectAdvice
+
+import static com.autonomousapps.AdviceHelper.actualProjectAdvice
+import static com.autonomousapps.AdviceHelper.emptyProjectAdviceFor
+import static com.autonomousapps.kit.gradle.Dependency.api
+
+final class AnnotationByDelegateProject extends AbstractProject {
+
+  final GradleProject gradleProject
+
+  AnnotationByDelegateProject() {
+    this.gradleProject = build()
+  }
+
+  private GradleProject build() {
+    return newGradleProjectBuilder()
+      .withSubproject('consumer') { s ->
+        s.sources = SOURCE_CONSUMER
+        s.withBuildScript { bs ->
+          bs.plugins = kotlin
+          bs.dependencies(
+            api('com.google.guava:guava:33.3.1-jre'),
+          )
+        }
+      }
+      .write()
+  }
+
+  private static final List<Source> SOURCE_CONSUMER = [
+    Source.kotlin(
+      '''\
+        package com.example.consumer
+  
+        import com.google.common.util.concurrent.Service
+  
+        // Service has methods annotated with @CanIgnoreReturnValue, a CLASS-retained annotation. by-delegation means
+        // kotlinc generates a MyService.class file which directly references that annotation.
+        class MyService(private val delegate: Service) : Service by delegate
+      '''
+    )
+      .withPath('com.example.MyService', 'MyService')
+      .build(),
+  ]
+
+  Set<ProjectAdvice> actualBuildHealth() {
+    return actualProjectAdvice(gradleProject)
+  }
+
+  final Set<ProjectAdvice> expectedBuildHealth = [
+    emptyProjectAdviceFor(':consumer'),
+  ]
+}
+
