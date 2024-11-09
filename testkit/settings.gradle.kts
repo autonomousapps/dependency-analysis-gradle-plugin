34d138e2ce389d48f2d5a83c2ad1a1a168b154cb
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-11-09 03:25:49 +0000

    Add toolchain plugin everywhere and unify plugin style
    > Configure project :testkit:gradle-testkit-plugin
    Using a toolchain installed via auto-provisioning, but having no toolchain repositories configured. This behavior is deprecated. Consider defining toolchain download repositories, otherwise the build might fail in clean environments; see https://docs.gradle.org/8.11-rc-3/userguide/toolchains.html#sub:download_repositoriesed artifacts.

--- testkit/settings.gradle.kts
+++ testkit/settings.gradle.kts
@@ -28,11 +28,13 @@ pluginManagement {
     id("com.gradle.develocity") version "3.18.2"
     id("com.gradle.plugin-publish") version "1.1.0"
     id("org.jetbrains.dokka") version "1.9.20"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
   }
 }
 
 plugins {
   id("com.gradle.develocity")
+  id("org.gradle.toolchains.foojay-resolver-convention")
 }
 
 dependencyResolutionManagement {
