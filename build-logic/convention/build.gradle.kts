0744efcb1703612d9cd324bfb37d237634f0a222
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-11-09 03:21:01 +0000

    Migrate to top-level Kotlin DSL instead of direct task configuration
    > Configure project :build-logic:convention
    w: build-logic/convention/build.gradle.kts:17:3: 'kotlinOptions(KotlinJvmOptionsDeprecated /* = KotlinJvmOptions */.() -> Unit): Unit' is deprecated. Please migrate to the compilerOptions DSL. More details are here: https://kotl.in/u1r8ln

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -1,7 +1,6 @@
 // Copyright (c) 2024. Tony Robalik.
 // SPDX-License-Identifier: Apache-2.0
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   id("java-gradle-plugin")
@@ -14,9 +13,9 @@ java {
   }
 }
 
-tasks.withType<KotlinCompile>().configureEach {
+kotlin {
   compilerOptions {
-    jvmTarget = JvmTarget.fromTarget(libs.versions.java.get())
+    jvmTarget = libs.versions.java.map(JvmTarget::fromTarget).get()
     freeCompilerArgs = listOf("-Xinline-classes", "-opt-in=kotlin.RequiresOptIn", "-Xsam-conversions=class")
   }
 }
