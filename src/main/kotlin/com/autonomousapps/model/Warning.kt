a1f3b5bdb83bc960b1e7eef8001f043adc9126b2
Tony Robalik <tony.robalik@gmail.com>
2024-11-14 14:11:53 -0800

    fix: ProjectAdvice is totally Comparable.

--- src/main/kotlin/com/autonomousapps/model/Warning.kt
+++ src/main/kotlin/com/autonomousapps/model/Warning.kt
@@ -1,14 +1,22 @@
 package com.autonomousapps.model
 
+import com.autonomousapps.internal.utils.LexicographicIterableComparator
 import com.squareup.moshi.JsonClass
 
 /** Warnings about the project for which the plugin cannot yet make universally actionable advice. */
 @JsonClass(generateAdapter = false)
-data class Warning(val duplicateClasses: Set<DuplicateClass>) {
+data class Warning(
+  val duplicateClasses: Set<DuplicateClass>,
+) : Comparable<Warning> {
 
   internal companion object {
     @JvmStatic
     fun empty() = Warning(emptySet())
+  }
+
+  override fun compareTo(other: Warning): Int {
+    return LexicographicIterableComparator<DuplicateClass>()
+      .compare(duplicateClasses, other.duplicateClasses)
   }
 
   fun isEmpty(): Boolean = duplicateClasses.isEmpty()
