917869382318d56ea3a255e815ccd8252148e02f
Tony Robalik <tony.robalik@gmail.com>
2024-11-14 14:11:53 -0800

    fix: ProjectAdvice is totally Comparable.

--- src/main/kotlin/com/autonomousapps/model/ProjectAdvice.kt
+++ src/main/kotlin/com/autonomousapps/model/ProjectAdvice.kt
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.autonomousapps.model
 
+import com.autonomousapps.internal.utils.LexicographicIterableComparator
 import com.squareup.moshi.JsonClass
 
 /** Collection of all advice for a single project, across all variants. */
@@ -19,6 +20,13 @@ data class ProjectAdvice(
   fun isEmpty(): Boolean = dependencyAdvice.isEmpty() && pluginAdvice.isEmpty() && warning.isEmpty()
   fun isNotEmpty(): Boolean = !isEmpty()
 
-  // TODO(tsr): this compareTo function violates the Comparable contract by only considering one field.
-  override fun compareTo(other: ProjectAdvice): Int = projectPath.compareTo(other.projectPath)
+  override fun compareTo(other: ProjectAdvice): Int {
+    return compareBy(ProjectAdvice::projectPath)
+      .thenBy(LexicographicIterableComparator()) { it.dependencyAdvice }
+      .thenBy(LexicographicIterableComparator()) { it.pluginAdvice }
+      .thenBy(LexicographicIterableComparator()) { it.moduleAdvice }
+      .thenBy(ProjectAdvice::warning)
+      .thenBy(ProjectAdvice::shouldFail)
+      .compare(this, other)
+  }
 }
