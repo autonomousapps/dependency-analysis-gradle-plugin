b6c40614caf0951a5c86091fa139888b38aafa9a
Gaëtan Muller <m.gaetan89@gmail.com>
2025-01-22 08:16:55 +0100

    Add AGP 8.10

--- src/functionalTest/groovy/com/autonomousapps/android/AbstractAndroidSpec.groovy
+++ src/functionalTest/groovy/com/autonomousapps/android/AbstractAndroidSpec.groovy
@@ -24,14 +24,15 @@ abstract class AbstractAndroidSpec extends AbstractFunctionalSpec {
   protected static final AGP_8_7 = AgpVersion.version('8.7.3')
   protected static final AGP_8_8 = AgpVersion.version('8.8.0')
   protected static final AGP_8_9 = AgpVersion.version('8.9.0-alpha09')
+  protected static final AGP_8_10 = AgpVersion.version('8.10.0-alpha01')
 
-  protected static final AGP_LATEST = AGP_8_9
+  protected static final AGP_LATEST = AGP_8_10
 
   /**
    * TODO(tsr): this doc is perpetually out of date.
    *
    * {@code AGP_8_0} represents the minimum stable _tested_ version. {@code AGP_8_8} represents the maximum stable
-   * _tested_ version. We also test against the latest alpha, {@code AGP_8_9} at time of writing. DAGP may work with
+   * _tested_ version. We also test against the latest alpha, {@code AGP_8_10} at time of writing. DAGP may work with
    * other versions of AGP, but they aren't tested, primarily for CI performance reasons.
    *
    * @see <a href="https://maven.google.com/web/index.html?#com.android.tools.build:gradle">AGP releases</a>
@@ -39,7 +40,7 @@ abstract class AbstractAndroidSpec extends AbstractFunctionalSpec {
   protected static final SUPPORTED_AGP_VERSIONS = [
     AGP_8_0,
     AGP_8_8,
-    AGP_8_9,
+    AGP_8_10,
   ]
 
   protected static List<AgpVersion> agpVersions(AgpVersion minAgpVersion = AgpVersion.AGP_MIN) {
