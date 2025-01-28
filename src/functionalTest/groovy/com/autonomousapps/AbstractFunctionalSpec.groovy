55377ea0acf57a46d38210280cd3ea59ff9ed60c
Tony Robalik <tony.robalik@gmail.com>
2025-01-28 10:19:30 -0800

    build: use Gradle 8.12.1.

--- src/functionalTest/groovy/com/autonomousapps/AbstractFunctionalSpec.groovy
+++ src/functionalTest/groovy/com/autonomousapps/AbstractFunctionalSpec.groovy
@@ -21,15 +21,15 @@ abstract class AbstractFunctionalSpec extends Specification {
   protected static final GRADLE_8_9 = GradleVersion.version('8.9')
   protected static final GRADLE_8_10 = GradleVersion.version('8.10.2')
   protected static final GRADLE_8_11 = GradleVersion.version('8.11.1')
+  protected static final GRADLE_8_12 = GradleVersion.version('8.12.1')
 
-  protected static final GRADLE_LATEST = GRADLE_8_11
+  protected static final GRADLE_LATEST = GRADLE_8_12
 
   // For faster CI times, we only test min + max. Testing all would be preferable, but we don't have till the heat death
   // of the universe.
   protected static final SUPPORTED_GRADLE_VERSIONS = [
     GradleVersions.minGradleVersion,
     GRADLE_LATEST,
-    //GRADLE_8_11,
   ]
 
   protected GradleProject gradleProject = null
