fc4b824ede957365d26d801a307857726fc5bb74
Tony Robalik <tony.robalik@gmail.com>
2025-02-07 13:21:16 -0800

    perf: intern more strings.

--- src/main/kotlin/com/autonomousapps/internal/ClassNames.kt
+++ src/main/kotlin/com/autonomousapps/internal/ClassNames.kt
@@ -4,7 +4,7 @@ package com.autonomousapps.internal
 
 internal object ClassNames {
 
-  fun canonicalize(className: String): String = className.replace('/', '.')
+  fun canonicalize(className: String): String = className.replace('/', '.').intern()
 
   // TODO(tsr): I think I can delete the slashy version but I'm not sure
   fun isEnum(superClassName: String?): Boolean = superClassName == "java.lang.Enum" || superClassName == "java/lang/Enum"
