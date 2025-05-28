9ec74286416ea09815743e1edd7f451eed56cb2c
Tony Robalik <tony.robalik@gmail.com>
2025-05-28 11:03:48 -0700

    test: moved DominanceTreeTest and related functions to graph-support module.

--- graph-support/src/test/kotlin/com/autonomousapps/graph/DominanceTreeTest.kt
+++ graph-support/src/test/kotlin/com/autonomousapps/graph/DominanceTreeTest.kt
@@ -1,18 +1,17 @@
 package com.autonomousapps.graph
 
-import com.autonomousapps.internal.utils.GraphAdapter.StringGraphContainer
-import com.autonomousapps.internal.utils.fromJson
-import com.autonomousapps.internal.utils.toJson
-import com.autonomousapps.test.textFromResource
+import com.autonomousapps.graph.utils.GraphAdapter.StringGraphContainer
+import com.autonomousapps.graph.utils.fromJson
+import com.autonomousapps.graph.utils.toJson
 import com.google.common.graph.Graph
 import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.Test
+import java.io.BufferedReader
+import java.io.InputStream
 import java.nio.file.Paths
 import kotlin.io.path.absolutePathString
 import kotlin.io.path.writeText
 
-// TODO(tsr): move to graph-support module?
-// TODO(tsr): move moshi string-graph stuff to test-only module?
 internal class DominanceTreeTest {
 
   @Test fun `can build dominance tree`() {
@@ -24,11 +23,12 @@ internal class DominanceTreeTest {
     val tree = DominanceTree(graph)
     // writeGoldenTree(tree) // use when re-generating the golden tree
 
-    // Then the only
+    // Then
     assertThat(tree.selfDominatingNodes()).containsExactly(tree.root)
     assertThat(tree.dominanceGraph).isEqualTo(goldenTree())
   }
 
+  @Suppress("unused")
   private fun writeGoldenTree(tree: DominanceTree<String>) {
     val output = Paths.get(".").normalize().resolve("golden-tree.json")
     println("output = ${output.absolutePathString()}")
@@ -40,4 +40,12 @@ internal class DominanceTreeTest {
     val json = textFromResource("golden-tree.json")
     return json.fromJson<StringGraphContainer>().graph
   }
+}
+
+private fun Any.textFromResource(resourcePath: String): String {
+  return streamFromResource(resourcePath).bufferedReader().use(BufferedReader::readText)
+}
+
+private fun Any.streamFromResource(resourcePath: String): InputStream {
+  return javaClass.classLoader.getResourceAsStream(resourcePath) ?: error("No resource at '$resourcePath'")
 }
---
+++ graph-support/src/test/kotlin/com/autonomousapps/graph/utils/moshi.kt
@@ -0,0 +1,86 @@
+@file:Suppress("UnstableApiUsage")
+
+package com.autonomousapps.graph.utils
+
+import com.google.common.graph.ElementOrder
+import com.google.common.graph.Graph
+import com.google.common.graph.GraphBuilder
+import com.google.common.graph.ImmutableGraph
+import com.squareup.moshi.*
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
+val MOSHI: Moshi by lazy {
+  Moshi.Builder()
+    .add(GraphAdapter())
+    .addLast(KotlinJsonAdapterFactory())
+    .build()
+}
+
+inline fun <reified T> JsonAdapter<T>.withNulls(withNulls: Boolean): JsonAdapter<T> {
+  return if (withNulls) {
+    serializeNulls()
+  } else {
+    this
+  }
+}
+
+inline fun <reified T> getJsonAdapter(withNulls: Boolean = false): JsonAdapter<T> {
+  return MOSHI.adapter(T::class.java).withNulls(withNulls)
+}
+
+inline fun <reified T> String.fromJson(): T {
+  return getJsonAdapter<T>().fromJson(this)!!
+}
+
+inline fun <reified T> T.toJson(withNulls: Boolean = false): String {
+  return getJsonAdapter<T>(withNulls).toJson(this)
+}
+
+@Suppress("unused")
+internal class GraphAdapter {
+
+  @ToJson fun stringGraphContainerToJson(graph: StringGraphContainer): StringGraphJson {
+    return StringGraphJson(
+      nodes = graph.graph.nodes().toSortedSet(),
+      edges = graph.graph.edges().asSequence().map { pair ->
+        pair.nodeU() to pair.nodeV()
+      }.toSortedSet()
+    )
+  }
+
+  @FromJson fun jsonToStringGraphContainer(json: StringGraphJson): StringGraphContainer {
+    val graphBuilder = newGraphBuilder<String>()
+    json.nodes.forEach { graphBuilder.addNode(it) }
+    json.edges.forEach { (source, target) -> graphBuilder.putEdge(source, target) }
+    val graph = graphBuilder.build()
+
+    return StringGraphContainer(graph)
+  }
+
+  private infix fun String.to(target: String) = StringEdgeJson(this, target)
+
+  @JsonClass(generateAdapter = false)
+  internal data class StringGraphContainer(val graph: Graph<String>)
+
+  @JsonClass(generateAdapter = false)
+  internal data class StringGraphJson(
+    val nodes: Set<String>,
+    val edges: Set<StringEdgeJson>,
+  )
+
+  @JsonClass(generateAdapter = false)
+  internal data class StringEdgeJson(val source: String, val target: String) : Comparable<StringEdgeJson> {
+    override fun compareTo(other: StringEdgeJson): Int {
+      return compareBy(StringEdgeJson::source)
+        .thenComparing(StringEdgeJson::target)
+        .compare(this, other)
+    }
+  }
+
+  private fun <T> newGraphBuilder(): ImmutableGraph.Builder<T> {
+    return GraphBuilder.directed()
+      .allowsSelfLoops(false)
+      .incidentEdgeOrder(ElementOrder.stable<T>())
+      .immutable()
+  }
+}
