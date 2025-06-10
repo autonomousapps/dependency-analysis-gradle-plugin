fe3063ea53a49c1385d3ec6c854f4087ac93cb52
Tony Robalik <tony.robalik@gmail.com>
2025-06-10 09:31:08 -0700

    chore: delete unused code.

--- build-logic/convention/src/main/kotlin/com/autonomousapps/convention/BaseConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/com/autonomousapps/convention/BaseConventionPlugin.kt
@@ -7,12 +7,10 @@ import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.provider.Provider
-import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.tasks.bundling.AbstractArchiveTask
 import org.gradle.api.tasks.testing.Test
 import org.gradle.jvm.toolchain.JavaLanguageVersion
 import org.gradle.plugins.signing.Sign
-import org.gradle.plugins.signing.SigningExtension
 import org.jetbrains.dokka.gradle.DokkaTask
 
 @Suppress("unused")
@@ -34,8 +32,6 @@ internal class BaseConventionPlugin(private val project: Project) {
     val publishedVersion = convention.publishedVersion
 
     val versionCatalog = extensions.getByType(VersionCatalogsExtension::class.java).named("libs")
-    val signing = extensions.getByType(SigningExtension::class.java)
-    val publishing = extensions.getByType(PublishingExtension::class.java)
     val javaVersion = JavaLanguageVersion.of(versionCatalog.findVersion("java").orElseThrow().requiredVersion)
 
     extensions.configure(JavaPluginExtension::class.java) { j ->
@@ -75,46 +71,6 @@ internal class BaseConventionPlugin(private val project: Project) {
       it.useJUnitPlatform()
     }
 
-    // afterEvaluate {
-    // val isPluginProject = pluginManager.hasPlugin("java-gradle-plugin")
-    // if (isPluginProject) {
-    //   // For plugin projects, the task name is different because one gets added automatically with a publication named
-    //   // "pluginMaven".
-    //   publishToMavenCentral.configure { t ->
-    //     t.dependsOn(tasks.named { it == "publishPluginMavenPublicationTo$SONATYPE_REPO_SUFFIX" })
-    //   }
-    // } else {
-    //   // Not a plugin project. We don't need this publication for the plugin itself, because it already exists.
-    //   publishing.publications.create(MAVEN_PUB_NAME, MavenPublication::class.java) { p ->
-    //     p.from(project.components.getAt("java"))
-    //   }
-    //
-    //   publishToMavenCentral.configure { t ->
-    //     t.dependsOn(tasks.named { it == "publish${MAVEN_PUB_NAME.capitalizeSafely()}PublicationTo$SONATYPE_REPO_SUFFIX" })
-    //   }
-    // }
-
-    // val promoteTask = tasks.register("promote", NexusPublishTask::class.java) {
-    //   it.onlyIf { !isSnapshot.get() }
-    //   it.configureWith(Credentials(project))
-    // }
-
-    // Used to set the description dynamically
-    // convention.setPublishToMavenCentral(publishToMavenCentral)
-
-    //pluginManager.withPlugin("com.gradle.plugin-publish")
-    // pluginManager.withPlugin("java-gradle-plugin") {
-    //   extensions.getByType(GradlePluginDevelopmentExtension::class.java).plugins.all { pluginConfig ->
-    //     publishToMavenCentral.configure { t ->
-    //       t.dependsOn(
-    //         // e.g. publishDependencyAnalysisPluginPluginMarkerMavenPublicationToSonatypeRepository
-    //         tasks.named { it == "publish${pluginConfig.name.capitalizeSafely()}PluginMarkerMavenPublicationTo$SONATYPE_REPO_SUFFIX" }
-    //         // "publish${pluginConfig.name.capitalizeSafely()}PluginMarkerMavenPublicationTo$SONATYPE_REPO_SUFFIX"
-    //       )
-    //     }
-    //   }
-    // }
-
     val isCi: Provider<Boolean> = providers
       .environmentVariable("CI")
       .orElse("false")
@@ -136,29 +92,4 @@ internal class BaseConventionPlugin(private val project: Project) {
       }
     }
   }
-
-  // internal companion object {
-  //   const val MAVEN_PUB_NAME = "maven"
-  //   const val SONATYPE_REPO_NAME = "sonatype"
-  //   val SONATYPE_REPO_SUFFIX = "${SONATYPE_REPO_NAME.capitalizeSafely()}Repository"
-  // }
 }
-
-// copied from StringsJVM.kt
-// private fun String.capitalizeSafely(locale: Locale = Locale.ROOT): String {
-//   if (isNotEmpty()) {
-//     val firstChar = this[0]
-//     if (firstChar.isLowerCase()) {
-//       return buildString {
-//         val titleChar = firstChar.titlecaseChar()
-//         if (titleChar != firstChar.uppercaseChar()) {
-//           append(titleChar)
-//         } else {
-//           append(this@capitalizeSafely.substring(0, 1).uppercase(locale))
-//         }
-//         append(this@capitalizeSafely.substring(1))
-//       }
-//     }
-//   }
-//   return this
-// }
