e1f5c9eb757d5cbdc41b7b07bc8448f599725667
Tony Robalik <tony.robalik@gmail.com>
2025-07-25 20:00:01 -0700

    chore: add some comments to prep for Dokka v2 migration.
    May not be ready from the maven publish plugin's perspective yet.

--- build-logic/convention/src/main/kotlin/com/autonomousapps/convention/LibKotlinConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/com/autonomousapps/convention/LibKotlinConventionPlugin.kt
@@ -26,6 +26,7 @@ public abstract class LibKotlinConventionPlugin : Plugin<Project> {
     extensions.getByType(MavenPublishBaseExtension::class.java).run {
       configure(
         KotlinJvm(
+          // TODO(tsr): dokkaHtml is from Dokka v1. Does not exist in Dokka v2. See gradle.properties.
           javadocJar = JavadocJar.Dokka("dokkaHtml"),
           sourcesJar = true,
         )
