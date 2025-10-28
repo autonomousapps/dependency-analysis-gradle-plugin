d4ce06d83ae56e8ab17d1474bdcfdae24600a469
Tony Robalik <trobalik@squareup.com>
2025-10-28 14:30:24 -0700

    fix: for builds with Gradle 9.1.0+, get the buildPath from the Gradle instance.
    Resolves https://github.com/autonomousapps/dependency-analysis-gradle-plugin/issues/1564

--- src/main/kotlin/com/autonomousapps/internal/utils/project/projects.kt
+++ src/main/kotlin/com/autonomousapps/internal/utils/project/projects.kt
@@ -7,30 +7,21 @@ import org.gradle.api.NamedDomainObjectProvider
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.component.ProjectComponentIdentifier
-import org.gradle.api.internal.project.ProjectInternal
 import org.gradle.api.provider.Provider
-import org.gradle.internal.build.BuildState
 
-/** Get the buildPath of the current build from the root component of the resolution result. */
+/** Get the buildPath of the current build. */
 internal fun Project.buildPath(configurationName: String): Provider<String> {
-  return buildPath(configurations.named(configurationName))
-}
-
-// TODO(tsr): update to use project.gradle.buildPath with Gradle 9.1.0
-internal fun Project.buildPath(configuration: Configuration): Provider<String> {
-  return if (GradleVersions.isAtLeastGradle900) {
-    project.provider { getIdentityPath() }
+  return if (GradleVersions.isAtLeastGradle910) {
+    provider { gradle.buildPath }
   } else {
-    configuration.incoming.resolutionResult.let {
-      it.rootComponent.map { root -> (root.id as ProjectComponentIdentifier).build.buildPath }
-    }
+    return buildPath(configurations.named(configurationName))
   }
 }
 
-// TODO(tsr): update to use project.gradle.buildPath with Gradle 9.1.0
+/** Get the buildPath of the current build. */
 internal fun Project.buildPath(configuration: NamedDomainObjectProvider<Configuration>): Provider<String> {
-  return if (GradleVersions.isAtLeastGradle900) {
-    project.provider { getIdentityPath() }
+  return if (GradleVersions.isAtLeastGradle910) {
+    provider { gradle.buildPath }
   } else {
     configuration.flatMap { c ->
       c.incoming.resolutionResult.let { result ->
@@ -39,10 +30,3 @@ internal fun Project.buildPath(configuration: NamedDomainObjectProvider<Configur
     }
   }
 }
-
-/**
- * @see <a href="https://github.com/gradle/gradle/pull/34007">Gradle PR #34007</a>
- * @see <a href="https://github.com/JetBrains/kotlin/blob/fb3d7ebc67466a2e1a30217337fc76df96d4732e/libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/utils/CurrentBuildIdentifier.kt#L15-L17">KGP</a>
- */
-internal fun Project.getIdentityPath(): String =
-  (project as ProjectInternal).services.get(BuildState::class.java).identityPath.toString()
