d1581299d0b0dfdd6bfde8952c533d3bf3ad610f
Tony Robalik <trobalik@squareup.com>
2025-11-19 16:31:41 -0800

    chore: disable deployment validation.

--- build-logic/convention/src/main/kotlin/com/autonomousapps/convention/DagpExtension.kt
+++ build-logic/convention/src/main/kotlin/com/autonomousapps/convention/DagpExtension.kt
@@ -39,7 +39,8 @@ public abstract class DagpExtension(
   }
 
   private fun setupPublishingRepo() {
-    mavenPublish.publishToMavenCentral(automaticRelease = true)
+    // Don't validate because I don't want to wait 15min for my build to finish.
+    mavenPublish.publishToMavenCentral(automaticRelease = true, validateDeployment = false)
     mavenPublish.signAllPublications()
 
     // We set the version explicitly because it seems there may be a race condition bug otherwise.
