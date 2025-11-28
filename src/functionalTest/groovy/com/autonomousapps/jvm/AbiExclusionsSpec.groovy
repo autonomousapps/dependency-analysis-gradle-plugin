5412dc34a967ce5a7c784a89defb48954b1f4717
Artyom Gabeev <artyom.gabeev@gmail.com>
2025-11-28 12:59:47 +0200

    feat: implement ABI inclusions filter

--- src/functionalTest/groovy/com/autonomousapps/jvm/AbiExclusionsSpec.groovy
+++ src/functionalTest/groovy/com/autonomousapps/jvm/AbiExclusionsSpec.groovy
@@ -2,8 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.autonomousapps.jvm
 
+import com.autonomousapps.jvm.projects.AbiClassAndAnnotationInclusionsProject
 import com.autonomousapps.jvm.projects.AbiExcludedSourceSetProject
 import com.autonomousapps.jvm.projects.AbiExclusionsProject
+import com.autonomousapps.jvm.projects.AbiPackageInclusionsCombinedProject
 
 import static com.autonomousapps.utils.Runner.build
 import static com.google.common.truth.Truth.assertThat
@@ -28,6 +30,36 @@ final class AbiExclusionsSpec extends AbstractJvmSpec {
   def "can exclude custom source set from ABI analysis (#gradleVersion)"() {
     given:
     def project = new AbiExcludedSourceSetProject()
+    gradleProject = project.gradleProject
+
+    when:
+    build(gradleVersion, gradleProject.rootDir, 'buildHealth')
+
+    then:
+    assertThat(project.actualBuildHealth()).containsExactlyElementsIn(project.expectedBuildHealth)
+
+    where:
+    gradleVersion << gradleVersions()
+  }
+
+  def "can include classes and annotations from different configuration points (#gradleVersion)"() {
+    given:
+    def project = new AbiClassAndAnnotationInclusionsProject()
+    gradleProject = project.gradleProject
+
+    when:
+    build(gradleVersion, gradleProject.rootDir, 'buildHealth')
+
+    then:
+    assertThat(project.actualBuildHealth()).containsExactlyElementsIn(project.expectedBuildHealth)
+
+    where:
+    gradleVersion << gradleVersions()
+  }
+
+  def "can include a package with excluding specific classes or annotations (#gradleVersion)"() {
+    given:
+    def project = new AbiPackageInclusionsCombinedProject()
     gradleProject = project.gradleProject
 
     when:
