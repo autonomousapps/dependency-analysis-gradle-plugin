aabc4bb2aefad931b384df42538c06e53c36899f
Tony Robalik <trobalik@squareup.com>
2026-02-26 11:06:57 -0800

    test: cover jvm + androidLibrary case for KMP modules.

--- src/functionalTest/groovy/com/autonomousapps/kmp/SimpleKmpSpec.groovy
+++ src/functionalTest/groovy/com/autonomousapps/kmp/SimpleKmpSpec.groovy
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.autonomousapps.kmp
 
+import com.autonomousapps.kmp.projects.AndroidAndJvmProject
 import com.autonomousapps.kmp.projects.AndroidTargetProject
 import com.autonomousapps.kmp.projects.JvmTargetProject
 
@@ -68,6 +69,66 @@ final class SimpleKmpSpec extends AbstractKmpSpec {
         Existing dependencies which should be modified to be as indicated:
           commonMain.dependencies {
             api("${AndroidTargetProject.OKIO}") (was commonMainImplementation)
+          }""".stripIndent()
+    )
+
+    where:
+    [gradleVersion, agpVersion] << gradleAgpMatrix()
+  }
+
+  def "can analyze a kmp project with android and jvm targets (#gradleVersion AGP #agpVersion)"() {
+    given:
+    def project = new AndroidAndJvmProject(agpVersion)
+    gradleProject = project.gradleProject
+
+    when:
+    def result = build(gradleVersion, gradleProject.rootDir, ':buildHealth')
+
+    then:
+    assertThat(project.actualBuildHealth()).containsExactlyElementsIn(project.expectedBuildHealth)
+
+    and:
+    assertThat(result.output).contains(
+      """\
+        Advice for :android
+        Unused dependencies which should be removed:
+          androidMain.dependencies {
+            api("${AndroidAndJvmProject.CAFFEINE}")
+          }
+        
+        These transitive dependencies should be declared directly:
+          androidHostTest.dependencies {
+            implementation("${AndroidAndJvmProject.KOTLIN_TEST}")
+          }
+        
+        Existing dependencies which should be modified to be as indicated:
+          commonMain.dependencies {
+            api("${AndroidAndJvmProject.OKIO}") (was commonMainImplementation)
+          }
+        
+        Advice for :jvm
+        Unused dependencies which should be removed:
+          commonMain.dependencies {
+            api("${AndroidAndJvmProject.KOTLINX_COROUTINES_CORE}")
+          }
+        
+        Existing dependencies which should be modified to be as indicated:
+          commonMain.dependencies {
+            api("${AndroidAndJvmProject.OKIO}") (was commonMainImplementation)
+          }
+          
+        Advice for :multiple
+        Unused dependencies which should be removed:
+          androidMain.dependencies {
+            api("${AndroidAndJvmProject.CAFFEINE}")
+          }
+          commonMain.dependencies {
+            api("${AndroidAndJvmProject.KOTLINX_COROUTINES_CORE}")
+          }
+        
+        Existing dependencies which should be modified to be as indicated:
+          commonMain.dependencies {
+            api("${AndroidAndJvmProject.OKIO}") (was commonMainImplementation)
           }""".stripIndent()
     )
 
---
+++ src/functionalTest/groovy/com/autonomousapps/kmp/projects/AndroidAndJvmProject.groovy
@@ -0,0 +1,297 @@
+// Copyright (c) 2025. Tony Robalik.
+// SPDX-License-Identifier: Apache-2.0
+package com.autonomousapps.kmp.projects
+
+import com.autonomousapps.AbstractProject
+import com.autonomousapps.kit.GradleProject
+import com.autonomousapps.kit.Source
+import com.autonomousapps.kit.gradle.kotlin.KotlinJvmTarget
+import com.autonomousapps.model.Advice
+import com.autonomousapps.model.ProjectAdvice
+
+import static com.autonomousapps.AdviceHelper.*
+import static com.autonomousapps.kit.gradle.Dependency.api
+import static com.autonomousapps.kit.gradle.Dependency.implementation
+
+final class AndroidAndJvmProject extends AbstractProject {
+
+  private static final String KOTLIN_VERSION = '2.2.21'
+
+  static final String CAFFEINE = 'com.github.ben-manes.caffeine:caffeine:3.2.3'
+  static final String KOTLIN_TEST = "org.jetbrains.kotlin:kotlin-test:$KOTLIN_VERSION"
+  static final String KOTLINX_COROUTINES_CORE = 'org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2'
+  static final String OKIO = 'com.squareup.okio:okio:3.16.4'
+
+  final GradleProject gradleProject
+
+  AndroidAndJvmProject(agpVersion) {
+    super(KOTLIN_VERSION, agpVersion as String)
+    this.gradleProject = build()
+  }
+
+  private GradleProject build() {
+    return newGradleProjectBuilder(GradleProject.DslKind.KOTLIN)
+      .withRootProject { r ->
+        r.withBuildScript { bs ->
+          bs.plugins(
+            plugins.dependencyAnalysis,
+            plugins.kotlinMultiplatformNoApply,
+            plugins.androidKmpLibNoApply,
+          )
+        }
+      }
+    // just an androidLibrary target
+      .withAndroidKmpLibProject('android') { s ->
+        s.sources = androidSources()
+        s.withBuildScript { bs ->
+          bs.plugins = androidKmpLibrary
+          bs.kotlin { k ->
+            k.androidLibrary { a ->
+              a.namespace = 'dagp.android'
+              a.compileSdk = 33
+              a.minSdk = 24
+              a.withHostTest()
+            }
+            k.sourceSets { sourceSets ->
+              sourceSets.commonMain { commonMain ->
+                commonMain.dependencies(
+                  api('com.squareup.okio:okio-bom:3.16.4').onKmpPlatform(),
+                  api('org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2'),
+                  implementation(OKIO),
+                )
+              }
+              sourceSets.androidMain { androidMain ->
+                androidMain.dependencies(
+                  api(CAFFEINE),
+                )
+              }
+              sourceSets.androidHostTest { androidHostTest ->
+                androidHostTest.dependencies(
+                  implementation('kotlin("test-junit")'),
+                  implementation('org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2'),
+                  implementation('org.assertj:assertj-core:3.27.7'),
+                  implementation('commons-io:commons-io:2.21.0'),
+                )
+              }
+            }
+          }
+        }
+      }
+    // just a jvm target
+      .withSubproject('jvm') { s ->
+        s.sources = jvmSources()
+        s.withBuildScript { bs ->
+          bs.plugins = kmpLibrary
+          bs.kotlin { k ->
+            k.jvmTarget = KotlinJvmTarget.default()
+            k.sourceSets { sourceSets ->
+              sourceSets.commonMain { commonMain ->
+                commonMain.dependencies(
+                  api('com.squareup.okio:okio-bom:3.16.4').onKmpPlatform(),
+                  api('org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2'),
+                  implementation(OKIO),
+                )
+              }
+            }
+          }
+        }
+      }
+    // both androidLibrary and jvm targets
+      .withAndroidKmpLibProject('multiple') { s ->
+        s.sources = multipleSources()
+        s.withBuildScript { bs ->
+          bs.plugins = androidKmpLibrary
+          bs.kotlin { k ->
+            // jvm target
+            k.jvmTarget = KotlinJvmTarget.default()
+
+            // androidLibrary target
+            k.androidLibrary { a ->
+              a.namespace = 'dagp.multiple'
+              a.compileSdk = 33
+              a.minSdk = 24
+            }
+            k.sourceSets { sourceSets ->
+              sourceSets.commonMain { commonMain ->
+                commonMain.dependencies(
+                  api('com.squareup.okio:okio-bom:3.16.4').onKmpPlatform(),
+                  api('org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2'),
+                  implementation(OKIO),
+                )
+              }
+              sourceSets.androidMain { androidMain ->
+                androidMain.dependencies(
+                  api(CAFFEINE),
+                )
+              }
+            }
+          }
+        }
+      }
+      .write()
+  }
+
+  private static List<Source> androidSources() {
+    return [
+      Source
+        .kotlin(
+          '''
+            package common.main
+            
+            import okio.Buffer
+            
+            abstract class CommonMain {
+              abstract fun usesOkio(): Buffer  
+            }
+          '''
+        )
+        .withSourceSet('commonMain')
+        .build(),
+      Source
+        .kotlin(
+          '''
+            package a.main
+            
+            import kotlinx.coroutines.CoroutineScope
+            import kotlinx.coroutines.Dispatchers
+            import kotlinx.coroutines.launch
+            import okio.BufferedSource
+            import okio.buffer
+            import okio.source
+            import java.io.File
+            
+            abstract class AndroidMain {
+              fun usesCoroutines() {
+                CoroutineScope(Dispatchers.Main).launch {
+                  println("Hi!")
+                }
+              }
+        
+              fun usesOkio(): BufferedSource {
+                return File(".").source().buffer()
+              }
+            }
+          '''
+        )
+        .withSourceSet('androidMain')
+        .build(),
+      Source
+        .kotlin(
+          '''
+            package a.host.test
+            
+            import kotlinx.coroutines.test.runTest
+            import kotlin.test.Test
+            import kotlin.test.assertTrue
+            
+            class HostTestCoroutines {
+              @Test fun test() = runTest {
+                assertTrue(true)
+              }
+            }
+          '''
+        )
+        .withSourceSet('androidHostTest')
+        .build(),
+      Source
+        .kotlin(
+          '''
+            package a.host.test
+            
+            import org.apache.commons.io.file.Counters
+            import org.assertj.core.api.Assertions
+            import org.junit.Assert
+            import kotlin.test.Test
+            
+            class HostTestCounters {
+              fun test() {
+                // commons-io
+                val counter = Counters.longCounter()
+        
+                // org.junit.Assert
+                Assert.assertTrue(true)
+        
+                // org.assertj.core.api.Assertions
+                Assertions.assertThat(true).isTrue()
+              }
+            }
+          '''
+        )
+        .withSourceSet('androidHostTest')
+        .build(),
+    ]
+  }
+
+  private static List<Source> jvmSources() {
+    return [
+      Source
+        .kotlin(
+          '''
+            package common.main
+            
+            import okio.Buffer
+            
+            abstract class CommonMain {
+              abstract fun usesOkio(): Buffer  
+            }
+          '''
+        )
+        .withSourceSet('commonMain')
+        .build(),
+    ]
+  }
+
+  private static List<Source> multipleSources() {
+    return [
+      Source
+        .kotlin(
+          '''
+            package common.main
+            
+            import okio.Buffer
+            
+            abstract class CommonMain {
+              abstract fun usesOkio(): Buffer  
+            }
+          '''
+        )
+        .withSourceSet('commonMain')
+        .build(),
+    ]
+  }
+
+  Set<ProjectAdvice> actualBuildHealth() {
+    return actualProjectAdvice(gradleProject)
+  }
+
+  private final Set<Advice> androidAdvice = [
+    // androidMainApi("com.github.ben-manes.caffeine:caffeine:3.2.3")
+    Advice.ofRemove(moduleCoordinates(CAFFEINE), 'androidMainApi'),
+    // commonMainApi("com.squareup.okio:okio:3.16.4") (was commonMainImplementation)
+    Advice.ofChange(moduleCoordinates(OKIO), 'commonMainImplementation', 'commonMainApi'),
+    // androidHostTestImplementation("org.jetbrains.kotlin:kotlin-test:2.2.21")
+    Advice.ofAdd(moduleCoordinates(KOTLIN_TEST), 'androidHostTestImplementation')
+  ]
+
+  //  Advice(com.squareup.okio:okio:3.16.4), fromConfiguration=commonMainImplementation, toConfiguration=commonMainApi),
+  //  Advice(org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2), fromConfiguration=commonMainApi, toConfiguration=null)
+  private final Set<Advice> jvmAdvice = [
+    Advice.ofRemove(moduleCoordinates(KOTLINX_COROUTINES_CORE), 'commonMainApi'),
+    Advice.ofChange(moduleCoordinates(OKIO), 'commonMainImplementation', 'commonMainApi'),
+  ]
+
+  //  Advice(com.github.ben-manes.caffeine:caffeine:3.2.3), fromConfiguration=androidMainApi, toConfiguration=null),
+  //  Advice(org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2), fromConfiguration=commonMainApi, toConfiguration=null)
+  //  Advice(com.squareup.okio:okio:3.16.4), fromConfiguration=commonMainImplementation, toConfiguration=commonMainApi),
+  private final Set<Advice> multipleAdvice = [
+    Advice.ofRemove(moduleCoordinates(CAFFEINE), 'androidMainApi'),
+    Advice.ofRemove(moduleCoordinates(KOTLINX_COROUTINES_CORE), 'commonMainApi'),
+    Advice.ofChange(moduleCoordinates(OKIO), 'commonMainImplementation', 'commonMainApi'),
+  ]
+
+  final Set<ProjectAdvice> expectedBuildHealth = [
+    projectAdviceForDependencies(':android', androidAdvice),
+    projectAdviceForDependencies(':jvm', jvmAdvice),
+    projectAdviceForDependencies(':multiple', multipleAdvice),
+  ]
+}
