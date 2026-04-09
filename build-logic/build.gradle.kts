d1b670d56d9219702528428aaa3dcf24157e33a8
Tony Robalik <tony.robalik@gmail.com>
2026-04-08 22:54:00 -0700

    chore: resolve kotlin-daemon-client issues.
    See https://kotlinlang.slack.com/archives/C19FD9681/p1775698198510429.

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,7 +1,7 @@
 // Copyright (c) 2026. Tony Robalik.
 // SPDX-License-Identifier: Apache-2.0
 plugins {
-  `kotlin-dsl`
+  id("org.jetbrains.kotlin.jvm") version "2.2.21" apply false
   alias(libs.plugins.dependencyAnalysis)
 }
 
