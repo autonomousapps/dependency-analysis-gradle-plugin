e6cb1fedf9fe27ca676325e14a01aaf9ac00fb82
Tony Robalik <tony.robalik@gmail.com>
2026-06-29 14:29:08 -0700

    test: add sadly non-failing test case for duplicate Android resource issue.
    https://github.com/autonomousapps/dependency-analysis-gradle-plugin/issues/1731

--- src/functionalTest/groovy/com/autonomousapps/android/ResSpec.groovy
+++ src/functionalTest/groovy/com/autonomousapps/android/ResSpec.groovy
@@ -4,9 +4,11 @@ package com.autonomousapps.android
 
 import com.autonomousapps.android.projects.*
 import org.gradle.testkit.runner.TaskOutcome
+import spock.lang.PendingFeature
 
 import static com.autonomousapps.advice.truth.BuildHealthSubject.buildHealth
-import static com.autonomousapps.utils.Runner.build
+import static com.autonomousapps.kit.GradleBuilder.build
+import static com.autonomousapps.kit.GradleBuilder.buildAndFail
 import static com.google.common.truth.Truth.assertAbout
 
 @SuppressWarnings("GroovyAssignabilityCheck")
@@ -194,6 +196,37 @@ final class ResSpec extends AbstractAndroidSpec {
 
     when:
     build(gradleVersion, gradleProject.rootDir, 'buildHealth')
+
+    then:
+    assertAbout(buildHealth())
+      .that(project.actualBuildHealth())
+      .isEquivalentIgnoringModuleAdviceAndWarnings(project.expectedBuildHealth)
+
+    where:
+    [gradleVersion, agpVersion] << gradleAgpMatrix()
+  }
+
+  // TODO(tsr): this test case demonstrates (or more accurately: fails to demonstrate) any issue when multiple Android
+  //  libraries provide a resource with the same name, and the consumer side only references that resource in an XML
+  //  file (so there's no qualified reference in code for more accurate detection).
+  // See e.g. https://developer.android.com/studio/projects/android-library#Considerations for information on resource
+  //  merging; and
+  // https://developer.android.com/reference/tools/gradle-api/8.3/null/com/android/build/api/dsl/CommonExtension#setResourcePrefix(kotlin.String)
+  //  for an AGP DSL that can help mitigate the problem (set unique prefixes everywhere and use them).
+  // This is annotated with `@PendingFeature` because the assertion is that the build fails (but the build succeeds). If
+  // there is ever any improvement in this scenario, this case will begin to really fail and then we can update the
+  // assertions.
+  @PendingFeature(reason = "https://github.com/autonomousapps/dependency-analysis-gradle-plugin/issues/1731")
+  def "handles duplicate res-by-res producers (#gradleVersion AGP #agpVersion)"() {
+    given:
+    def project = new ResByResProject(agpVersion)
+    gradleProject = project.gradleProject
+
+    when:
+    buildAndFail(gradleVersion, gradleProject.rootDir, 'buildHealth')
+    // The output is identical
+//    build(gradleVersion, gradleProject.rootDir, ':app:reason', '--id', ':res1')
+//    build(gradleVersion, gradleProject.rootDir, ':app:reason', '--id', ':res2')
 
     then:
     assertAbout(buildHealth())
